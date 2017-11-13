a43d4e9198c35f8fd0f270c1bb3e47f00fdb0631
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-13 15:20:34 +0700

    Add default user data

--- react-native/instagram/screens/account/reducer.js
+++ react-native/instagram/screens/account/reducer.js
@@ -5,7 +5,11 @@ import { REHYDRATE } from "redux-persist/lib/constants";
 import { Types } from "./actions";
 
 export const INITIAL_STATE = Immutable({
-  data: []
+  biography: null,
+  full_name: null,
+  id: null,
+  profile_pic_url: null,
+  username: null
 });
 
 const updatePersist = (state, action) => {
@@ -15,6 +19,21 @@ const updatePersist = (state, action) => {
   });
 };
 
+const loadAccountData = (state, action) => {
+  // FIX ME: Fixed data for now, no auth yet
+  return state.merge({
+    type: action.type,
+    biography: "We love hard so we fall hard",
+    full_name: "Huy Nguyen",
+    id: 2250323280,
+    profile_pic_url:
+      "https://instagram.fdad3-1.fna.fbcdn.net/t51.2885-19/s150x150/13642880_138562836574694_572060052_a.jpg",
+    username: "huyb1991"
+  });
+};
+
 export const accountReducer = createReducer(INITIAL_STATE, {
-  [REHYDRATE]: updatePersist
+  [REHYDRATE]: updatePersist,
+
+  [Types.LOAD_ACCOUNT_DATA]: loadAccountData
 });
