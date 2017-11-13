9d92493271647908adabf1d6053b51a3267ef5b7
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-13 11:09:55 +0700

    Add image to list data

--- react-native/instagram/screens/home/reducer.js
+++ react-native/instagram/screens/home/reducer.js
@@ -3,6 +3,7 @@ import { createReducer } from "reduxsauce";
 import { REHYDRATE } from "redux-persist/lib/constants";
 
 import { Types } from "./actions";
+import { Types as UploadTypes } from "../upload/actions";
 
 export const INITIAL_STATE = Immutable({
   data: []
@@ -27,9 +28,21 @@ const addData = (state, action) => {
     .updateIn(["data"], arr => arr.concat([state.data.length]));
 };
 
+const addPhotoToList = (state, action) => {
+  const singlePhoto = {
+    url: action.response.downloadURL,
+    like: 0,
+    comments: []
+  };
+  return state
+    .merge({ type: action.type })
+    .updateIn(["data"], arr => arr.concat([singlePhoto]));
+};
+
 export const homeReducer = createReducer(INITIAL_STATE, {
   [REHYDRATE]: updatePersist,
 
   [Types.GET_HOME_DATA_REQUEST]: getHomeDataRequest,
+  [UploadTypes.UPLOAD_PHOTO_SUCCESS]: addPhotoToList,
   [Types.ADD_DATA]: addData
 });
