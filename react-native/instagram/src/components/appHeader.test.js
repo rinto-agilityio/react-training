704df56d13efc83a21c77e17cb76e6e867576985
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-17 15:28:49 +0700

    Update jest test setup file

--- react-native/instagram/src/components/appHeader.test.js
+++ react-native/instagram/src/components/appHeader.test.js
@@ -1,7 +1,3 @@
-import "react-native";
-import React from "react";
-import renderer from "react-test-renderer";
-
 import AppHeader from "./AppHeader";
 
 test("AppHeader renders correctly", () => {
