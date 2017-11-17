ac35e7e99a66136b9105429d44deda0cda2a9270
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-17 15:25:18 +0700

    Add test case check default state

--- react-native/instagram/src/components/comment.test.js
+++ react-native/instagram/src/components/comment.test.js
@@ -1,8 +1,12 @@
 import "react-native";
 import React from "react";
 import renderer from "react-test-renderer";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 import Comment from "./Comment";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 // Mocking props
 const owner = {
@@ -13,4 +17,15 @@ test("Comment renders correctly", () => {
   const tree = renderer.create(<Comment owner={owner} />).toJSON();
 
   expect(tree).toMatchSnapshot();
+});
+
+test("Comment call function when click", () => {
+  const defaultState = { text: "" };
+
+  const submitComment = jest.fn();
+  const wrapper = shallow(
+    <Comment owner={owner} submitComment={submitComment} />
+  );
+
+  expect(wrapper.state()).toEqual(defaultState);
 });
