90076a14f558681cc613179c8e6e652092933af1
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-17 15:32:47 +0700

    Update unit test structure

--- react-native/instagram/src/components/appHeader.test.js
+++ react-native/instagram/src/components/appHeader.test.js
@@ -1,7 +1,9 @@
 import AppHeader from "./AppHeader";
 
-test("AppHeader renders correctly", () => {
-  const tree = renderer.create(<AppHeader />).toJSON();
+describe("AppHeader component", () => {
+  it("AppHeader renders correctly", () => {
+    const tree = renderer.create(<AppHeader />).toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
 });
--- react-native/instagram/src/components/comment.test.js
+++ react-native/instagram/src/components/comment.test.js
@@ -5,19 +5,21 @@ const owner = {
   profile_pic_url: "avatar_url.png"
 };
 
-test("Comment renders correctly", () => {
-  const tree = renderer.create(<Comment owner={owner} />).toJSON();
+describe("Comment component", () => {
+  it("Renders correctly", () => {
+    const tree = renderer.create(<Comment owner={owner} />).toJSON();
 
-  expect(tree).toMatchSnapshot();
-});
+    expect(tree).toMatchSnapshot();
+  });
 
-test("Comment call function when click", () => {
-  const defaultState = { text: "" };
+  it("Comment call function when click", () => {
+    const defaultState = { text: "" };
 
-  const submitComment = jest.fn();
-  const wrapper = shallow(
-    <Comment owner={owner} submitComment={submitComment} />
-  );
+    const submitComment = jest.fn();
+    const wrapper = shallow(
+      <Comment owner={owner} submitComment={submitComment} />
+    );
 
-  expect(wrapper.state()).toEqual(defaultState);
+    expect(wrapper.state()).toEqual(defaultState);
+  });
 });
