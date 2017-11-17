b1de912e0d9e9bcd78f85468566d40e89f46975d
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-17 15:15:40 +0700

    Update snapshot for Comment component

--- react-native/instagram/src/components/comment.test.js
+++ react-native/instagram/src/components/comment.test.js
@@ -4,12 +4,12 @@ import renderer from "react-test-renderer";
 
 import Comment from "./Comment";
 
-test("Comment renders correctly", () => {
-  // Mocking props
-  const owner = {
-    profile_pic_url: "avatar_url.png"
-  };
+// Mocking props
+const owner = {
+  profile_pic_url: "avatar_url.png"
+};
 
+test("Comment renders correctly", () => {
   const tree = renderer.create(<Comment owner={owner} />).toJSON();
 
   expect(tree).toMatchSnapshot();
