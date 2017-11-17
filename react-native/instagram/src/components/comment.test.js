f734af74d4a9c3def68b036cae15aee33859faac
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-17 15:59:31 +0700

    Add test case when change text

--- react-native/instagram/src/components/comment.test.js
+++ react-native/instagram/src/components/comment.test.js
@@ -1,15 +1,17 @@
 import Comment from "./Comment";
 
 describe("Comment component", () => {
-  let component, treeSnapshot;
+  let component, treeSnapshot, textInput;
   const defaultState = { text: "" },
     owner = {
       profile_pic_url: "avatar_url.png"
     };
 
-  beforeEach(function() {
+  beforeEach(() => {
     component = shallow(<Comment owner={owner} />);
     treeSnapshot = renderer.create(<Comment owner={owner} />).toJSON();
+
+    textInput = component.find("TextInput");
   });
 
   it("Renders correctly", () => {
@@ -18,5 +20,27 @@ describe("Comment component", () => {
 
   it("Render default state", () => {
     expect(component.state()).toEqual(defaultState);
+  });
+
+  it("Renders input field with placeholder", () => {
+    const expectedPlaceholder = "Add a comment";
+    expect(textInput).toHaveLength(1);
+    expect(textInput.props().placeholder).toEqual(expectedPlaceholder);
+  });
+
+  describe("When text changes", () => {
+    const newTextValue = "new Text Value";
+
+    beforeEach(() => {
+      textInput.simulate("changeText", newTextValue);
+    });
+
+    it("Updates component state", () => {
+      expect(component.state().text).toEqual(newTextValue);
+    });
+
+    it("Renders updated text", () => {
+      expect(textInput.props().text).toEqual(newTextValue);
+    });
   });
 });
