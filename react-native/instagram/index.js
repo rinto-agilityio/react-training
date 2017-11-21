0a6b4339a2f63eda143b013e29b149ce51641d86
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-21 16:36:59 +0700

    Template load storybook

--- react-native/instagram/index.js
+++ react-native/instagram/index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from "react-native";
 import App from "./src/App";
+import StorybookUI from "./src/storybook";
 
-AppRegistry.registerComponent("instagram", () => App);
+// AppRegistry.registerComponent("instagram", () => App);
+AppRegistry.registerComponent("instagram", () => StorybookUI);
