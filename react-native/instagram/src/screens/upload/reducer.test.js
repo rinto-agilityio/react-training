76b98dd024a04173165b8f7164fe70060cddb8d5
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-22 10:40:41 +0700

    Add unit test for upload reducer

--- react-native/instagram/src/screens/upload/reducer.test.js
+++ react-native/instagram/src/screens/upload/reducer.test.js
@@ -1,7 +1,82 @@
-import reducer from "./reducer";
+import { Types } from "./actions";
+import { REHYDRATE } from "redux-persist/lib/constants";
+
+import { uploadReducer, INITIAL_STATE } from "./reducer";
 
 describe("Upload reducer", () => {
-  it("Init test case for Upload reducer", () => {
-    expect(true).toEqual(true);
+  it("Should handle REHYDRATE", () => {
+    expect(uploadReducer(INITIAL_STATE, { type: REHYDRATE })).toEqual(
+      INITIAL_STATE.merge({
+        type: REHYDRATE,
+        isUploading: false
+      })
+    );
+  });
+
+  it("Should handle UPLOAD_PHOTO_REQUEST", () => {
+    expect(
+      uploadReducer(INITIAL_STATE, {
+        type: Types.UPLOAD_PHOTO_REQUEST
+      })
+    ).toEqual(
+      INITIAL_STATE.merge({
+        type: Types.UPLOAD_PHOTO_REQUEST,
+        isUploading: true
+      })
+    );
+  });
+
+  it("Should handle UPLOAD_PHOTO_CANCEL", () => {
+    expect(
+      uploadReducer(INITIAL_STATE, {
+        type: Types.UPLOAD_PHOTO_CANCEL
+      })
+    ).toEqual(
+      INITIAL_STATE.merge({
+        type: Types.UPLOAD_PHOTO_CANCEL,
+        isUploading: false
+      })
+    );
+  });
+
+  it("Should handle UPLOAD_PHOTO_FAILURE", () => {
+    // Mocking error from api || firebase
+    const error = {
+      message: "Upload failed"
+    };
+
+    expect(
+      uploadReducer(INITIAL_STATE, {
+        type: Types.UPLOAD_PHOTO_FAILURE,
+        error: error
+      })
+    ).toEqual(
+      INITIAL_STATE.merge({
+        type: Types.UPLOAD_PHOTO_FAILURE,
+        data: null,
+        error: error
+      })
+    );
+  });
+
+  it("Should handle UPLOAD_PHOTO_SUCCESS", () => {
+    // Mocking response from api || firebase
+    const response = {
+      downloadUrl: "download_image_url"
+    };
+
+    expect(
+      uploadReducer(INITIAL_STATE, {
+        type: Types.UPLOAD_PHOTO_SUCCESS,
+        response: response
+      })
+    ).toEqual(
+      INITIAL_STATE.merge({
+        type: Types.UPLOAD_PHOTO_SUCCESS,
+        data: response,
+        isUploading: false,
+        error: null
+      })
+    );
   });
 });
