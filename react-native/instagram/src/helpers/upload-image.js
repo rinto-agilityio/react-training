58499ba6ac291ab383d222e71cbb8e9172aa2839
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-28 16:25:50 +0700

    #13 Add .env

--- react-native/instagram/src/helpers/upload-image.js
+++ react-native/instagram/src/helpers/upload-image.js
@@ -9,12 +9,12 @@ window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
 window.Blob = Blob
 
 const fbConfig = {
-  apiKey: 'AIzaSyCZpGh86wxsZM8H_PyZVKsk_MnjopqYDyY',
-  authDomain: 'instagram-rn.firebaseapp.com',
-  databaseURL: 'https://instagram-rn.firebaseio.com',
-  projectId: 'instagram-rn',
-  storageBucket: 'gs://instagram-rn.appspot.com',
-  messagingSenderId: '201897093582'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 }
 
 const fbApp = firebase.initializeApp(fbConfig),
