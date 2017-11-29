5809f70d389b695a50d047572c4f2e3232758517
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-11-29 15:41:02 +0700

    Add new test case when user click on comment icon

--- react-native/instagram/src/screens/home/components/singlePhoto.test.js
+++ react-native/instagram/src/screens/home/components/singlePhoto.test.js
@@ -2,11 +2,12 @@ import { photos } from '../../../test/__mocks__/sample-data'
 import SinglePhoto from './SinglePhoto'
 
 describe('SinglePhoto component', () => {
+  let component
   const item = photos[0],
     item2 = photos[1] // This data has some likes
 
   beforeEach(() => {
-    const component = shallow(<SinglePhoto item={item} />)
+    component = shallow(<SinglePhoto item={item} />)
   })
 
   it('Renders correctly without like', () => {
@@ -19,5 +20,13 @@ describe('SinglePhoto component', () => {
     const treeDOM = renderer.create(<SinglePhoto item={item2} />).toJSON()
 
     expect(treeDOM).toMatchSnapshot()
+  })
+
+  it('Change state after click comment icon', () => {
+    const initialState = component.state()
+
+    component.instance().toogleCommentBox()
+
+    expect(initialState.showComment).toEqual(!component.state().showComment)
   })
 })
