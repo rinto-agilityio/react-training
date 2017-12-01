bd37b160d560fdf2f279dd420ec034b0cfba0d29
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-01 14:47:35 +0700

    Add storybook for PostAuthor

--- react-native/instagram/src/screens/home/components/postAuthor.stories.js
+++ react-native/instagram/src/screens/home/components/postAuthor.stories.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Text } from 'react-native'
 import { storiesOf } from '@storybook/react-native'
 
+// Config
 import { users } from '@test/__mocks__/sample-data'
+
+// Component
 import PostAuthor from './PostAuthor'
 
-storiesOf('SinglePost', module).add('PostAuthor', () => (
-  <Text>Todo storybook</Text>
+const { profile_pic_url, username } = users[0]
+
+storiesOf('Components', module).add('PostAuthor', () => (
+  <PostAuthor profile_pic_url={profile_pic_url} username={username} />
 ))
--- react-native/instagram/storybook/stories/index.js
+++ react-native/instagram/storybook/stories/index.js
@@ -2,11 +2,19 @@
 // Require new components
 // Add textcenter for all stories
 
+// Layout
 require('@common/components/appHeader.stories')
+
+// Components
 require('@common/components/comment.stories')
+require('@screens/home/components/postPhoto.stories')
+require('@screens/home/components/postAuthor.stories')
 
 require('@screens/account/components/info.stories')
 require('@screens/account/components/photos.stories')
 require('@screens/home/components/commentList.stories')
 require('@screens/home/components/postAction.stories')
 require('@screens/home/components/singlePhoto.stories')
+
+// Screens
+require('@screens/account/components/accountScreen.stories')
