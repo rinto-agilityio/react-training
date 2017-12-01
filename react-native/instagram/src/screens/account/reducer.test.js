28069c917fb2f1e69adb62213c8c91fb28948660
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-01 10:00:38 +0700

    Add unit test for first time install app, no data in localStorage

--- react-native/instagram/src/screens/account/reducer.test.js
+++ react-native/instagram/src/screens/account/reducer.test.js
@@ -5,19 +5,34 @@ import { users } from '@test/__mocks__/sample-data'
 import { accountReducer, INITIAL_STATE } from './reducer'
 
 describe('Account reducer', () => {
-  it('Should handle REHYDRATE', () => {
-    expect(
-      accountReducer(INITIAL_STATE, {
-        type: REHYDRATE,
-        payload: {
-          account: {}
-        }
-      })
-    ).toEqual(
-      INITIAL_STATE.merge({
-        type: REHYDRATE
-      })
-    )
+  describe('Should handle REHYDRATE', () => {
+    it('Should load data from localStorage', () => {
+      expect(
+        accountReducer(INITIAL_STATE, {
+          type: REHYDRATE,
+          payload: {
+            account: {}
+          }
+        })
+      ).toEqual(
+        INITIAL_STATE.merge({
+          type: REHYDRATE
+        })
+      )
+    })
+
+    it('Should get fixed data for first time', () => {
+      expect(
+        accountReducer(INITIAL_STATE, {
+          type: REHYDRATE
+        })
+      ).toEqual(
+        INITIAL_STATE.merge({
+          type: REHYDRATE,
+          ...users[0]
+        })
+      )
+    })
   })
 
   it('Should handle LOAD_ACCOUNT_DATA', () => {
