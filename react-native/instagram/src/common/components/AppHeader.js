dadf57652363158e69b128d8c27079a6fc9a5b15
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-04 14:01:27 +0700

    Add constants for app

--- react-native/instagram/src/common/components/AppHeader.js
+++ react-native/instagram/src/common/components/AppHeader.js
@@ -1,10 +1,16 @@
+// Libs
 import React from 'react'
 import { View, Text } from 'react-native'
+
+// Helpers
+import { APP_NAME } from '@constants'
+
+// Styles
 import { styles } from './styles/AppHeaderStyles'
 
 const AppHeader = () => (
   <View style={styles.wrapper}>
-    <Text style={styles.brand}>Instagram</Text>
+    <Text style={styles.brand}>{APP_NAME}</Text>
   </View>
 )
 
