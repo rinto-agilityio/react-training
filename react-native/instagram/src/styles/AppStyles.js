4760f69cf79127ce1919bec9ef35e5b5ada8dc25
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-04 17:34:24 +0700

    Update style for fix tabBar on android simulator

--- react-native/instagram/src/styles/AppStyles.js
+++ react-native/instagram/src/styles/AppStyles.js
@@ -1,11 +1,13 @@
 import { StyleSheet } from 'react-native'
 import Themes from '@themes'
 
+// TODO: Remove align and justifyContent to make it work on android simulator for now
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Themes.bgColor,
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: Themes.bgColor
+    // alignItems: 'center',
+    // justifyContent: 'center'
   }
 })
