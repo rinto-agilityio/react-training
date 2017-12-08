d3fd465f2c4a1e7bd95373fd257e08edcabab2c0
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-08 17:15:56 +0700

    #22 Split container and components for AccountScreen

--- react-native/instagram/src/screens/account/components/AccountScreen.js
+++ react-native/instagram/src/screens/account/components/AccountScreen.js
@@ -6,27 +6,48 @@ import PropTypes from 'prop-types'
 // Components
 import Info from './Info'
 import Photos from './Photos'
+import Icon from '@common/components/Icon'
+
+// Helpers
+import { getAccountPhotos } from '@helpers/account-data'
 
 // Styles
 import CommonStyles from '@themes/common'
+import Icons from '@themes/icons'
 import { styles } from './styles/AccountScreenStyles'
 
-const AccountScreen = ({ owner, photos }) => (
-  <View style={CommonStyles.layoutColumn}>
-    <View style={styles.info}>
-      <Info owner={owner} />
-    </View>
-    <Photos photos={photos} />
-  </View>
-)
+// TODO: Change to function/stateless component and add defaultProps, PropTypes
+class AccountScreen extends React.Component {
+  static navigationOptions = {
+    tabBarIcon: () => (
+      <Icon
+        style={CommonStyles.tabBarIcon}
+        source={Icons.avatar}
+      />
+    )
+  }
 
-AccountScreen.defaultProps = {
-  photos: []
-}
+  /**
+   * Update data for current user
+   * Filter photos of current user from all photos in the system of all users
+   */
+  componentDidMount() {
+    this.props.loadAccountData()
+  }
 
-AccountScreen.propTypes = {
-  owner: PropTypes.object.isRequired,
-  photos: PropTypes.array
+  render() {
+    const { accountData, allPhotos } = this.props
+    const myPhotos = getAccountPhotos(allPhotos, accountData)
+
+    return (
+      <View style={CommonStyles.layoutColumn}>
+        <View style={styles.info}>
+          <Info owner={accountData} />
+        </View>
+        <Photos photos={myPhotos} />
+      </View>
+    )
+  }
 }
 
 export default AccountScreen
--- react-native/instagram/src/screens/account/containers/index.js
+++ react-native/instagram/src/screens/account/containers/index.js
@@ -1,46 +1,12 @@
 // Libs
-import React from 'react'
-import { Text, View } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 // Helpers
-import { getAccountPhotos } from '@helpers/account-data'
 import { Creators as AccountActionCreators } from '../actions'
 
 // Components
 import AccountScreen from '../components/AccountScreen'
-import Icon from '@common/components/Icon'
-
-// Styles
-import CommonStyles from '@themes/common'
-import Icons from '@themes/icons'
-
-class AccountContainer extends React.Component {
-  static navigationOptions = {
-    tabBarIcon: () => (
-      <Icon
-        style={CommonStyles.tabBarIcon}
-        source={Icons.avatar}
-      />
-    )
-  }
-
-  /**
-   * Update data for current user
-   * Filter photos of current user from all photos in the system of all users
-   */
-  componentDidMount() {
-    this.props.loadAccountData()
-  }
-
-  render() {
-    const { accountData, allPhotos } = this.props
-    const myPhotos = getAccountPhotos(allPhotos, accountData)
-
-    return <AccountScreen owner={accountData} photos={myPhotos} />
-  }
-}
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators(
@@ -58,4 +24,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AccountContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(AccountScreen)
