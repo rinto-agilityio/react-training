b28120bdd4e4688cde40b4313df2ed2d769bc3e6
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-08 09:58:33 +0700

    #20 Update code convention

--- react-native/instagram/src/screens/home/components/PostAction.js
+++ react-native/instagram/src/screens/home/components/PostAction.js
@@ -7,7 +7,13 @@ import PropTypes from 'prop-types'
 import { styles } from './styles/PostActionStyles'
 import CommonStyles from '@themes/common'
 
-const PostAction = ({ postId, userId, isLiked, toggleLike, toggleCommentBox }) => {
+const PostAction = ({
+  postId,
+  userId,
+  isLiked,
+  toggleLike,
+  toggleCommentBox
+}) => {
   // User like or remove liked a photo
   const _handleTouchIconLike = () => {
     toggleLike({
@@ -16,15 +22,28 @@ const PostAction = ({ postId, userId, isLiked, toggleLike, toggleCommentBox }) =
     })
   }
 
-  const likeIcon = isLiked ? require('@assets/icons/liked.png') : require('@assets/icons/like.png')
+  const likeIcon = isLiked
+    ? require('@assets/icons/liked.png')
+    : require('@assets/icons/like.png')
 
   return (
     <View style={CommonStyles.layoutRow}>
-      <TouchableHighlight className="icon-like" onPress={() => _handleTouchIconLike()} underlayColor={'transparent'}>
+      <TouchableHighlight
+        className="icon-like"
+        onPress={() => _handleTouchIconLike()}
+        underlayColor={'transparent'}
+      >
         <Image style={styles.icon} source={likeIcon} />
       </TouchableHighlight>
-      <TouchableHighlight className="icon-comment" onPress={() => toggleCommentBox()} underlayColor={'transparent'}>
-        <Image style={styles.icon} source={require('@assets/icons/comment.png')} />
+      <TouchableHighlight
+        className="icon-comment"
+        onPress={() => toggleCommentBox()}
+        underlayColor={'transparent'}
+      >
+        <Image
+          style={styles.icon}
+          source={require('@assets/icons/comment.png')}
+        />
       </TouchableHighlight>
     </View>
   )
