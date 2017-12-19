228b8189ddfe496d7861511a2b8bcfe4f84756ac
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-19 17:50:24 +0700

    Validate reducer with ESLint

--- react-native/instagram/src/screens/home/reducers/index.js
+++ react-native/instagram/src/screens/home/reducers/index.js
@@ -8,92 +8,84 @@ import { Types } from '../actions'
 import { Types as UploadTypes } from '@screens/upload/actions'
 import { generateFeeds } from '@test/__mocks__/generate-data'
 
-export const INITIAL_STATE = Immutable({
-  data: []
-})
+const INITIAL_STATE = Immutable({
+    data: []
+  }),
+  updatePersist = (state, action) => {
+    /**
+     * Redux persist throw error if no data in this case
+     * So, set default empty data for the first time install app
+     */
+    const { payload } = action,
+      homePayload = payload && payload.home ? payload.home : {}
 
-const updatePersist = (state, action) => {
-  /**
-   * Redux persist throw error if no data in this case
-   * So, set default empty data for the first time install app
-   */
-  const { payload } = action,
-        homePayload = payload && payload.home ? payload.home : {}
-
-  return state.merge({
-    type: action.type,
-    ...homePayload
-  })
-}
-
-const getHomeDataRequest = (state, action) => {
-  return state.merge({
+    return state.merge({
+      type: action.type,
+      ...homePayload
+    })
+  },
+  getHomeDataRequest = (state, action) => state.merge({
     type: action.type
-  })
-}
-
-const addPhotoToList = (state, action) => {
-  return state.merge({
+  }),
+  addPhotoToList = (state, action) => state.merge({
     type: action.type,
     data: [action.response].concat(state.data)
+  }),
+  addComment = (state, action) => {
+    const comment = Immutable(action.comment),
+      postIdx = state.data.findIndex(item => item.id === comment.postId),
+      newData = state.data.updateIn([postIdx, 'comments'], arr => arr.concat([comment.merge({ id: Date.now() }).without('postId')]))
+
+    return state.merge({
+      type: action.type,
+      data: newData
+    })
+  },
+  toggleLike = (state, action) => {
+    let newLikes
+    const notFoundIndex = -1,
+      { postId, userId } = action.data,
+      postIdx = state.data.findIndex(item => item.id === postId),
+      { likes } = state.data[postIdx],
+      likeIdx = likes.findIndex(item => item === userId)
+
+    if (likeIdx !== notFoundIndex) {
+      newLikes = likes.filter(item => item !== userId)
+    } else {
+      newLikes = likes.concat([userId])
+    }
+
+    const newData = state.data.setIn([postIdx, 'likes'], newLikes)
+
+    return state.merge({
+      type: action.type,
+      data: newData
+    })
+  },
+
+  /**
+   * This is fake function get fake data to test render performance
+   * @param {object} state - previous state
+   * @param {string} action - Action type
+   * @returns {object} - New state with fake items
+   */
+  fakeGetHomeDataRequest = (state, action) => {
+    const numberItems = 1000
+
+    return state.merge({
+      type: action.type,
+      data: generateFeeds(numberItems)
+    })
+  },
+  homeReducer = createReducer(INITIAL_STATE, {
+    [REHYDRATE]: updatePersist,
+
+    // [Types.GET_HOME_DATA_REQUEST]: fakeGetHomeDataRequest, // This for performance testing only
+    [Types.GET_HOME_DATA_REQUEST]: getHomeDataRequest,
+    [Types.ADD_COMMENT]: addComment,
+    [Types.TOGGLE_LIKE]: toggleLike,
+
+    [UploadTypes.UPLOAD_PHOTO_SUCCESS]: addPhotoToList
   })
-}
 
-const addComment = (state, action) => {
-  const comment = Immutable(action.comment),
-        postIdx = state.data.findIndex(item => item.id === comment.postId),
-        newData = state.data.updateIn([postIdx, 'comments'], arr =>
-          arr.concat([comment.merge({id: Date.now() }).without('postId')])
-        )
-
-  return state.merge({
-    type: action.type,
-    data: newData
-  })
-}
-
-const toggleLike = (state, action) => {
-  let newLikes
-  const { postId, userId } = action.data,
-        postIdx = state.data.findIndex(item => item.id === postId),
-        likes = state.data[postIdx].likes,
-        likeIdx = likes.findIndex(item => item === userId)
-
-  if (likeIdx != -1) {
-    newLikes = likes.filter(item => item !== userId)
-  } else {
-    newLikes = likes.concat([userId])
-  }
-
-  const newData = state.data.setIn([postIdx, 'likes'], newLikes)
-
-  return state.merge({
-    type: action.type,
-    data: newData
-  })
-}
-
-/**
- * This is fake function get fake data to test render performance
- * @param {object} state
- * @param {string} action
- */
-const fakeGetHomeDataRequest = (state, action) => {
-  const numberItems = 1000
-
-  return state.merge({
-    type: action.type,
-    data: generateFeeds(numberItems)
-  })
-}
-
-export const homeReducer = createReducer(INITIAL_STATE, {
-  [REHYDRATE]: updatePersist,
-
-  // [Types.GET_HOME_DATA_REQUEST]: fakeGetHomeDataRequest, // This for performance testing only
-  [Types.GET_HOME_DATA_REQUEST]: getHomeDataRequest,
-  [Types.ADD_COMMENT]: addComment,
-  [Types.TOGGLE_LIKE]: toggleLike,
-
-  [UploadTypes.UPLOAD_PHOTO_SUCCESS]: addPhotoToList
-})
+export { INITIAL_STATE, homeReducer }
