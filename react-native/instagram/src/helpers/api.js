f518af57af9e86c3ea0b7b8f68db410ce6b4388f
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-19 16:22:08 +0700

    Add helper wrapp response

---
+++ react-native/instagram/src/helpers/api.js
@@ -0,0 +1,28 @@
+// Libs
+import { fbDatabase } from '@configs/firebase'
+
+// Helpers
+import { photos } from '@test/__mocks__/sample-data'
+import { getListAsArray } from './firebase'
+
+const dbRef = fbDatabase.ref('feeds'),
+
+  /**
+   * Wrapp response with status for successful case
+   * @param {[array, object]} response - Response data
+   * @return {object} - object with success status
+   */
+  wrapperResponseSuccessful = response => ({
+    response,
+    ok: true
+  }),
+
+  /**
+   * Wrapp response with status for failure case
+   * @param {object} error - Response data
+   * @return {object} - object with fail status
+   */
+  wrapperResponseFailure = error => ({
+    error,
+    ok: false
+  })
