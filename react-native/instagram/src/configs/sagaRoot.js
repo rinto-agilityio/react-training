5f5e0b2f9229c00a582a82f641da99b859bbfe51
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-20 10:34:09 +0700

    Add saga for Upload screen

--- react-native/instagram/src/configs/sagaRoot.js
+++ react-native/instagram/src/configs/sagaRoot.js
@@ -1,8 +1,9 @@
 import homeSaga from '@screens/home/sagas'
+import uploadSaga from '@screens/upload/sagas'
 
 /**
  * @returns {object} sagaRoot
  */
 export default function* sagaRoot() {
-  yield [homeSaga()]
+  yield [homeSaga(), uploadSaga()]
 }
