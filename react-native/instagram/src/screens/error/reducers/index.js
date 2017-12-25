3009373cb6fbe7f509dd950473a973134e0204f6
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-25 09:25:22 +0700

    Update redux persist for REHYDRATE action

--- react-native/instagram/src/screens/error/reducers/index.js
+++ react-native/instagram/src/screens/error/reducers/index.js
@@ -1,26 +1,43 @@
 // Libs
 import Immutable from 'seamless-immutable'
 import { createReducer } from 'reduxsauce'
+import { REHYDRATE } from 'redux-persist/lib/constants'
 
 // Helpers
 import Types from '../actions'
 
 const INITIAL_STATE = Immutable({
     message: null,
-    type: null
+    apiType: null
+  }),
+
+  /**
+   * For error reducer, persist is clear all errors
+   * @param {object} state - Old state
+   * @param {object} action - Action types
+   * @returns {object} - Reset all errors
+   */
+  updatePersist = (state, action) => Immutable(state).merge({
+    type: action.type,
+    message: null,
+    apiType: null
   }),
 
   addError = (state, action) => Immutable(state).merge({
-    type: action.error.type,
+    type: action.type,
+    apiType: action.error.type,
     message: action.error.message
   }),
 
   clearError = (state, action) => Immutable(state).merge({
-    type: null,
+    type: action.type,
+    apiType: null,
     message: null
   }),
 
   errorReducer = createReducer(INITIAL_STATE, {
+    [REHYDRATE]: updatePersist,
+
     [Types.ADD_ERROR]: addError,
     [Types.CLEAR_ERROR]: clearError
   })
