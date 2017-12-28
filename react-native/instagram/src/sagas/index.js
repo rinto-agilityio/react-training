31d32839190a97ac081f793342c66fa199ae8f31
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2017-12-28 15:31:29 +0700

    Update saga root and split listen changes

--- react-native/instagram/src/sagas/index.js
+++ react-native/instagram/src/sagas/index.js
@@ -1,59 +1,16 @@
-// Libs
-import { take, put, fork } from 'redux-saga/effects'
-import { eventChannel } from 'redux-saga'
-
-// Helpers
-import { fbDatabase } from '@configs/firebase'
-import { getListAsArray } from '@helpers/firebase'
-import { Types as HomeActionTypes } from '@screens/home/actions'
-
 // Sagas
 import homeSaga from '@screens/home/sagas'
 import uploadSaga from '@screens/upload/sagas'
+import listChanges from './listen-changes'
 
 /**
- * Listen snapshot value change
- * Update app by dispatch an action get whole data succeed
- * @returns {function} Return function
- */
-function createEventChannel() {
-  const listener = eventChannel(emit => {
-    fbDatabase
-      .ref('feeds')
-      .on('value', snapshot => emit(snapshot))
-
-    return () => fbDatabase.ref('feeds').off(listener)
-  })
-
-  return listener
-}
-
-/**
- * Listen firebase event when add new items to 'feeds'
- * @return {function} Dispatch an action to add new item to list
- */
-function* updateFeeds() {
-  const updateChannel = createEventChannel()
-
-  while (true) {
-    const item = yield take(updateChannel)
-
-    if (item) {
-      yield put({
-        type: HomeActionTypes.GET_HOME_DATA_SUCCESS,
-        response: getListAsArray(item)
-      })
-    }
-  }
-}
-
-/**
+ * Saga root for app
  * @returns {object} sagaRoot
  */
 export default function* sagaRoot() {
   return yield [
     homeSaga(),
     uploadSaga(),
-    fork(updateFeeds)
+    listChanges()
   ]
 }
