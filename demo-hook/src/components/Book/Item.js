dce3122ac284d63bb25d975dcfbf25234cbf9d0d
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-01-15 15:05:26 +0700

    Split BookItem component

--- demo-hook/src/components/Book/Item.js
+++ demo-hook/src/components/Book/Item.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  border-bottom: 1px solid #eee;
+  display: grid;
+  grid-template-columns: 160px auto;
+  grid-gap: 20px;
+`
+const ThumbnailWrapper = styled.div``
+const Thumbnail = styled.img`
+  max-width: 100%;
+`
+const Info = styled.div`
+  display: inline-block;
+`
+const Title = styled.h3``
+const Description = styled.p``
+const Author = styled.p``
 
 const BookItem = ({ item }) => (
-  <div>{item.title}</div>
+  <Wrapper>
+    <ThumbnailWrapper>
+      <Thumbnail src={item.cover} alt={item.title} />
+    </ThumbnailWrapper>
+    <Info>
+      <Title>{item.title}</Title>
+      <Author>Author: {item.author.name}</Author>
+      <Description>{item.desc}</Description>
+    </Info>
+  </Wrapper>
 )
 
 export default BookItem
