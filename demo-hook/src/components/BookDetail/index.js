45303b841771bb845fdf705140abecd6750ae141
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-01-15 15:37:35 +0700

    Update component for BookDetail

---
+++ demo-hook/src/components/BookDetail/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+const BookDetail = () => {
+  const [book, setBook] = useState({})
+
+  // Fetching data from api for book detail
+  useEffect(() => {
+  })
+
+  return (
+    <div>Book Details</div>
+  )
+}
+
+export default BookDetail
