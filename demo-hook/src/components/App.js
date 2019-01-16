220f7c088f3cc04225581aaa4f6aa7c355c86d75
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-01-16 10:05:03 +0700

    Update style for app wrapper

--- demo-hook/src/components/App.js
+++ demo-hook/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
 
 // Themes
 import { GlobalStyle } from '../theme/globalStyle'
@@ -13,6 +14,11 @@ import Header from './Header'
 const BookList = React.lazy(() => import('./BookList'))
 const BookDetail = React.lazy(() => import('./BookDetail'))
 const Author = React.lazy(() => import('./Author'))
+
+// Similar to container
+const AppWrapper = styled.div`
+  padding: 1rem;
+`
 
 // Split router
 const AppRouter = () => (
@@ -32,9 +38,11 @@ const App = () => (
             <React.Fragment>
               <GlobalStyle />
               <Header />
-              <Suspense fallback={<Loading />}>
-                <AppRouter />
-              </Suspense>
+              <AppWrapper>
+                <Suspense fallback={<Loading />}>
+                  <AppRouter />
+                </Suspense>
+              </AppWrapper>
             </React.Fragment>
           </Router>
         </ThemeProvider>
