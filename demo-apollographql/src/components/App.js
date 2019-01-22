e4e95073d0c3d7f3f2c2b5da3efdcf76d27e7e80
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-01-22 11:02:08 +0700

    Do example request with GraphQL with ApolloClient

--- demo-apollographql/src/components/App.js
+++ demo-apollographql/src/components/App.js
@@ -3,27 +3,42 @@ import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-boost'
 import gql from 'graphql-tag'
 
+// Components
+import { Query } from 'react-apollo'
+
 
 const client = new ApolloClient({
   uri: "https://48p1r2roz4.sse.codesandbox.io",
 })
 
-client
-  .query({
-      query: gql`
-        {
-          rates(currency: "USD") {
-            currency
-          }
-        }
-      `
-  })
-  .then(results => console.log(results))
+const QUERY_RATES = gql`
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`
 
+const ExchangeRates = () => (
+  <Query query={QUERY_RATES}>
+    {( {loading, error, data }) => {
+      if (loading) return <p>Loading...</p>
+      if (error) return <p>Error !!!</p>
+
+      return data.rates.map(({ currency, rate }) => (
+        <div key={currency}>
+          <p>{currency}: {rate}</p>
+        </div>
+      ))
+    }}
+  </Query>
+)
 
 const App = () => (
   <ApolloProvider client={client}>
     <h1>React App</h1>
+    <ExchangeRates />
   </ApolloProvider>
 )
 
