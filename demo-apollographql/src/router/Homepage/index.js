2053ed87ef11fa839f08a2745b26d90ebbe3ef5f
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-01-23 14:32:15 +0700

    Update component for Homepage

--- demo-apollographql/src/router/Homepage/index.js
+++ demo-apollographql/src/router/Homepage/index.js
@@ -1,27 +1,60 @@
 import React from 'react'
 import { Query } from 'react-apollo'
+import styled from 'styled-components'
 
 // GraphQL
-import { QUERY_POST } from './graphql'
+import {
+  QUERY_POST,
+  QUERY_TOP_AUTHORS,
+} from './graphql'
 
 // Components
+import Container from '../../components/Layout/Container'
 import ListPost from '../../components/ListPost'
+import ListAuthor from '../../components/ListAuthor';
 
-const Homepage = () => (
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 300px;
+  grid-column-gap: 1rem;
+`
+const MainContent = styled.section``
+const Sidebar = styled.aside``
+
+const LatestPost = () => (
   <Query query={QUERY_POST}>
-    {( {loading, error, data, refetch }) => {
+    {( {loading, error, data }) => {
       if (loading) return <p>Loading...</p>
       if (error) return <p>Error !!!</p>
 
-      return (
-        <>
-          <h1>Latest post</h1>
-          <button onClick={() => refetch()}>refetch!</button>
-          <ListPost posts={data.posts} />
-        </>
-      )
+      return <ListPost posts={data.posts} />
     }}
   </Query>
+)
+
+const TopAuthor = () => (
+  <Query query={QUERY_TOP_AUTHORS}>
+    {( {loading, error, data }) => {
+      if (loading) return <p>Loading...</p>
+      if (error) return <p>Error !!!</p>
+
+      return <ListAuthor authors={data.authors} />
+    }}
+  </Query>
+)
+
+const Homepage = () => (
+  <Container>
+    <Wrapper>
+      <MainContent>
+        <h1>Latest post</h1>
+        <LatestPost />
+      </MainContent>
+      <Sidebar>
+        <TopAuthor />
+      </Sidebar>
+    </Wrapper>
+  </Container>
 )
 
 export default Homepage
