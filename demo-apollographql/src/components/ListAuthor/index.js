120fccb6b9c647962e045625ad6b1a4df921c80a
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-01-23 14:32:30 +0700

    Update data and component for ListAuthor and ListPost

--- demo-apollographql/src/components/ListAuthor/index.js
+++ demo-apollographql/src/components/ListAuthor/index.js
@@ -1,21 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const AuthorWrapper = styled.div``
+const ListAuthorWrapper = styled.section`
+  background-color: #fff;
+  padding: 1rem;
+  border-radius: 3px;
+`
+const Title = styled.h3`
+  margin: 1rem auto;
+  text-align: center;
+`
+const AuthorWrapper = styled.div`
+  display: flex;
+  margin-bottom: 2rem;
+`
 const AuthorPhotoWrapper = styled.div``
-const AuthorName = styled.h2``
+const AuthorPhoto = styled.img`
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+`
+const AuthorName = styled.h3`
+  margin: 0.5rem 1rem;
+`
 
 const ListAuthor = ({ authors }) => (
-  <>
+  <ListAuthorWrapper>
+    <Title>Top Authors</Title>
     {authors.map(author => (
       <AuthorWrapper key={author.id}>
         <AuthorPhotoWrapper>
-          <img src={author.photo} alt={author.name} />
+          <AuthorPhoto src={author.photo} alt={author.name} />
         </AuthorPhotoWrapper>
         <AuthorName>{author.name}</AuthorName>
       </AuthorWrapper>
     ))}
-  </>
+  </ListAuthorWrapper>
 )
 
 export default ListAuthor
--- demo-apollographql/src/components/ListPost/index.js
+++ demo-apollographql/src/components/ListPost/index.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const ListPostWrapper = styled.section`
+  display: grid;
+  grid-template-rows: auto;
+  grid-row-gap: 20px;
+
+`
+const SinglePostWrapper = styled.article`
+  background-color: #fff;
+  padding: 20px;
+  border: 1px solid rgba(0,0,0,.09);
+  border-radius: 3px;
+  box-shadow: 0 1px 4px rgba(0,0,0,.03);
+`
 
 const ListPost = ({ posts }) => (
-  <>
+  <ListPostWrapper>
     {posts.map(post => (
-      <section key={post.id}>
+      <SinglePostWrapper key={post.id}>
         <h2>{post.title}</h2>
         <p>{post.content}</p>
-      </section>
+      </SinglePostWrapper>
     ))}
-  </>
+  </ListPostWrapper>
 )
 
 export default ListPost
