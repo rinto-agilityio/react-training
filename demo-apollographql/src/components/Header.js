55374b4d8e7493eb3c2aa7df3616a0ebee46f3d5
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-02-12 09:55:42 +0700

    Apply material-ui component

--- demo-apollographql/src/components/Header.js
+++ demo-apollographql/src/components/Header.js
@@ -1,19 +1,42 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
 
-import {
-  HeaderWrapper,
-  SiteTitle,
-  SiteTitleLink,
-} from './Header.style'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
 
-const Header = () => (
-  <HeaderWrapper>
-    <SiteTitle>
-      <SiteTitleLink to="/" title="React-hook demo">
-        GraphQL & React
-      </SiteTitleLink>
-    </SiteTitle>
-  </HeaderWrapper>
+const styles = theme => ({
+  header: {
+    marginBottom: '1rem',
+  },
+  paper: {
+    borderRadius: 0,
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  link: {
+    textDecoration: 'none',
+  }
+})
+
+const Header = ({ classes }) => (
+  <Grid container className={classes.header}>
+    <Grid item xs={12}>
+      <Paper className={classes.paper}>
+        <Typography component="h1" variant="h3">
+          <Link
+            to="/"
+            title="React-hook demo"
+            className={classes.link}
+          >
+            GraphQL & React
+          </Link>
+        </Typography>
+      </Paper>
+    </Grid>
+  </Grid>
 )
 
-export default Header
+export default withStyles(styles)(Header)
