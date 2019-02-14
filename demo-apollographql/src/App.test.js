d89ef2480e2dd6d4c991d9b503b7be09d1285ffa
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-02-14 11:26:11 +0700

    Add note for issue testing React lazy

--- demo-apollographql/src/App.test.js
+++ demo-apollographql/src/App.test.js
@@ -4,6 +4,10 @@ import { render, wait } from 'react-testing-library'
 // Components
 import App from './App'
 
+/**
+ * Note: This use other test lib
+ * For Enzyme package, check this issue: https://github.com/airbnb/enzyme/issues/1917
+ */
 describe('Components', () => {
   describe('<App />', () => {
     it('Render with Supense and Lazy components without error', async () => {
