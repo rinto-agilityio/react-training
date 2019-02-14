a734246ff5457d91d057b1d098e3e28e10697436
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-02-14 11:47:14 +0700

    Refactor routers component by remore withStyles HOC

--- demo-apollographql/src/routers.js
+++ demo-apollographql/src/routers.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { withStyles } from '@material-ui/core/styles'
+import { Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
 
 // Components
 import Grid from '@material-ui/core/Grid'
@@ -10,28 +10,25 @@ const Homepage = React.lazy(() => import('./pages/Homepage'))
 const Post = React.lazy(() => import('./pages/Post'))
 const Author = React.lazy(() => import('./pages/Author'))
 
-const styles = theme => ({
-  mainContainer: {
-    padding: '1rem',
-  }
+const StyledGrid = styled(Grid)({
+  padding: '1rem'
 })
 
 // Router config
-const AppRouter = ({ classes }) => (
-  <Grid
+const AppRouter = () => (
+  <StyledGrid
     container
     direction="row"
     justify="space-between"
     alignItems="flex-start"
     spacing={40}
-    className={classes.mainContainer}
   >
     <Switch>
       <Route path="/" exact component={props => <Homepage {...props}/>} />
       <Route path="/post/:slug" component={props => <Post {...props} />} />
       <Route path="/author/:id" component={props => <Author {...props} />} />
     </Switch>
-  </Grid>
+  </StyledGrid>
 )
 
-export default withStyles(styles)(AppRouter)
+export default AppRouter
