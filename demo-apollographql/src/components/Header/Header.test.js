8d99425be357b1596b03fdf59d48b33ca6a7e6ec
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-02-18 15:44:01 +0700

    Add test snapshot

--- demo-apollographql/src/components/Header/Header.test.js
+++ demo-apollographql/src/components/Header/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { MemoryRouter } from 'react-router-dom'
+import renderer from 'react-test-renderer'
 
 // Components
 import Header from './index'
@@ -18,6 +19,18 @@ describe('Components', () => {
       )
 
       expect(wrapper.find(StyledLink).filter({ to: '/'}).render().text()).toEqual(appTitle)
+    })
+
+    it('Render correct snapshot', () => {
+      const tree = renderer
+      .create(
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      )
+      .toJSON()
+
+      expect(tree).toMatchSnapshot()
     })
   })
 })
