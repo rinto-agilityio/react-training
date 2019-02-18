c687c518b1e9507037c0dd63e634f1b2c18229e3
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-02-18 14:15:04 +0700

    Add babel and Jest config

---
+++ demo-apollographql/jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!src/{apollo,theme,stories}/**.*",
+    "!src/**/graphql.js",
+    "!src/**/*.{style,story}.{js,jsx}"
+  ],
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  setupFiles: ['<rootDir>/src/setupTests.js'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'json', 'jsx'],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).js?(x)'],
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+
+  // Indicates whether each individual test should be reported during the run
+  verbose: false,
+}
