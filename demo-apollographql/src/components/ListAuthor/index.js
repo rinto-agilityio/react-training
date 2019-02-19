fa4221c65e91a58aa28cbfa6547489f7bb23e381
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-02-19 11:40:35 +0700

    Refactor shared ListAuthor and TopAuthors component

--- demo-apollographql/src/components/ListAuthor/index.js
+++ demo-apollographql/src/components/ListAuthor/index.js
@@ -2,45 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 // Components
-import Typography from '@material-ui/core/Typography'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import ListItemText from '@material-ui/core/ListItemText'
 import Avatar from '@material-ui/core/Avatar'
-import { StyledCard, StyledLink } from './ListAuthor.style'
+import { StyledLink } from './ListAuthor.style'
 
-const ListAuthor = ({ title, authors }) => (
-  <StyledCard>
-    <Typography component="h3" variant="h5">
-      {title}
-    </Typography>
-
-    <List>
-      {authors.map(author => (
-        <StyledLink
-          key={author.id}
-          to={`/author/${author.id}`}
-        >
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar src={author.photo} alt={author.name} />
-            </ListItemAvatar>
-            <ListItemText>{author.name}</ListItemText>
-          </ListItem>
-        </StyledLink>
-      ))}
-    </List>
-  </StyledCard>
+const ListAuthor = ({ authors }) => (
+  <List>
+    {authors.map(author => (
+      <StyledLink
+        key={author.id}
+        to={`/author/${author.id}`}
+      >
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar src={author.photo} alt={author.name} />
+          </ListItemAvatar>
+          <ListItemText>{author.name}</ListItemText>
+        </ListItem>
+      </StyledLink>
+    ))}
+  </List>
 )
 
 ListAuthor.defaultProps = {
-  title: 'Top Authors',
   authors: []
 }
 
 ListAuthor.propTypes = {
-  title: PropTypes.string.isRequired,
   authors: PropTypes.array.isRequired
 }
 
--- demo-apollographql/src/pages/Homepage/components/TopAuthors/index.js
+++ demo-apollographql/src/pages/Homepage/components/TopAuthors/index.js
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types'
 // Components
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
 import ListAuthor from '../../../../components/ListAuthor'
 
-const TopAuthors = ({ authors }) => (
+const TopAuthors = ({ title, authors }) => (
   <Grid item xs={12} md={3}>
+    <Typography component="h3" variant="h5">
+      {title}
+    </Typography>
     <Paper>
       <ListAuthor authors={authors} />
     </Paper>
@@ -16,10 +20,12 @@ const TopAuthors = ({ authors }) => (
 )
 
 TopAuthors.defaultProps = {
+  title: 'Top Authors',
   authors: []
 }
 
 TopAuthors.propTypes = {
+  title: PropTypes.string.isRequired,
   authors: PropTypes.array.isRequired
 }
 
