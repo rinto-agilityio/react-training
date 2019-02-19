a74e02119ee6fd10299c98ced2ff2e30efc50d7b
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-02-19 16:05:46 +0700

    Update defaultProps and propTypes for SinglePost component

--- demo-apollographql/src/pages/Post/components/SinglePost/index.js
+++ demo-apollographql/src/pages/Post/components/SinglePost/index.js
@@ -1,33 +1,32 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
 
 // Components
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 
-const styles = ({
-  paper: {
-    padding: '1rem'
-  },
-  author: {
-    fontStyle: 'italic',
-    borderTop: '1px solid #555',
-  }
-})
-
-const SinglePost = ({ title, fullContent, author, classes }) => (
-  <Paper className={classes.paper}>
+const SinglePost = ({ title, fullContent, author }) => (
+  <Paper>
     <Typography component="h1" variant="h3">{title}</Typography>
     <div dangerouslySetInnerHTML={{__html: fullContent}} />
 
     <Typography
       component="h3"
       variant="subtitle2"
-      className={classes.author}
     >
       {author.name}
     </Typography>
   </Paper>
 )
 
-export default withStyles(styles)(SinglePost)
+SinglePost.defaultProps = {
+  author: {},
+}
+
+SinglePost.propTypes = {
+  title: PropTypes.string,
+  fullContent: PropTypes.string,
+  author: PropTypes.object.isRequired,
+}
+
+export default SinglePost
