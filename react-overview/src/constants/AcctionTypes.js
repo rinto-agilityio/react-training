836690ecb3e57207f1aa441dc32d73cdf6ff8546
Rin To <rin.to@asnet.com.vn>
2019-04-08 15:10:15 +0700

    Updated account context

---
+++ react-overview/src/constants/AcctionTypes.js
@@ -0,0 +1,3 @@
+export const TYPES = {
+  UPDATE_PROFILE: 'UPDATE_PROFILE'
+}
--- react-overview/src/contexts/ContextWrapper.js
+++ react-overview/src/contexts/ContextWrapper.js
@@ -1,31 +1,31 @@
-import React from 'react';
+// import React from 'react';
 
-export const themes = {
-  light: {
-    foreground: '#000000',
-    background: '#eeeeee',
-    name: 'light style',
-  },
-  dark: {
-    foreground: '#ffffff',
-    background: '#222222',
-    name: 'dark style'
-  },
-};
+// export const themes = {
+//   light: {
+//     foreground: '#000000',
+//     background: '#eeeeee',
+//     name: 'light style',
+//   },
+//   dark: {
+//     foreground: '#ffffff',
+//     background: '#222222',
+//     name: 'dark style'
+//   },
+// };
 
-export const { Provider, Consumer } = React.createContext({
-  theme: themes.dark, // default value,
-  toggleTheme: () => {},
-});
+// export const { Provider, Consumer } = React.createContext({
+//   theme: themes.dark, // default value,
+//   toggleTheme: () => {},
+// });
 
-export function ContextWrapper(WrappedComponent) {
-  return function Wrapper(props) {
-    return(
-      <Consumer>
-        { value => (
-          <WrappedComponent {...props} {...value} />
-        )}
-      </Consumer>
-    )
-  }
-}
+// export function ContextWrapper(WrappedComponent) {
+//   return function Wrapper(props) {
+//     return(
+//       <Consumer>
+//         { value => (
+//           <WrappedComponent {...props} {...value} />
+//         )}
+//       </Consumer>
+//     )
+//   }
+// }
