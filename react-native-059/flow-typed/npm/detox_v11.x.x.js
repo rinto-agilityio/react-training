f0f60dcb6987bd0818f9307332987e9b467a524a
Huy Van Nguyen <huy.nguyenvan@asnet.com.vn>
2019-04-23 15:45:35 +0700

    Feature/new optimize

---
+++ react-native-059/flow-typed/npm/detox_v11.x.x.js
@@ -0,0 +1,13 @@
+declare module 'detox' {
+  declare module.exports: any;
+}
+
+declare module 'detox/runners/jest/adapter' {
+  declare module.exports: any;
+}
+
+declare var device: any
+
+declare var element: any
+
+declare var by: any
