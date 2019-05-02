6c57d4d91f5eadfd53e15b07a70502ed25b92ac7
Quan Do <quan.do@asnet.com.vn>
2019-05-02 13:51:39 +0700

    Create style for component button

---
+++ pchef/packages/components/src/Button/styles.js
@@ -0,0 +1,32 @@
+// Lib
+import { StyleSheet } from 'react-native';
+
+// Create style for button
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3e94f6',
+    padding: 10
+  },
+  title: {
+    color: '#fff',
+  },
+  smallWeb: {
+    width: 100,
+    height: 40,
+  },
+  largeWeb: {
+    width: 200,
+    height: 40,
+  },
+  smallMobile: {
+    width: 100,
+  },
+  largeMobile: {
+    width: '100%',
+  },
+});
+
+
+export default styles;
