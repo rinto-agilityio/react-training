43248138944fa3cfc1027bf498173a94f65779df
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-02 16:00:09 +0700

    Config firebase FireStore

---
+++ pchef/server/src/config/database.js
@@ -0,0 +1,12 @@
+const firebase = require('firebase')
+
+// Firebase config
+firebase.initializeApp({
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+})
+
+module.exports = firebase.firestore()
