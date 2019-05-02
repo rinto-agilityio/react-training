886ce51111ff3cfa589cac43fceee6adf0d661b3
Quan Do <quan.do@asnet.com.vn>
2019-05-02 15:32:11 +0700

    Update style for button

--- pchef/packages/components/src/Button/styles.js
+++ pchef/packages/components/src/Button/styles.js
@@ -1,26 +1,31 @@
 // Lib
 import { StyleSheet } from 'react-native';
 
+// Themes
+import COLORS from '../../../themes/Colors';
+
 // Create style for button
 const styles = StyleSheet.create({
   button: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#3e94f6',
+    backgroundColor: COLORS.green,
     padding: 10
   },
   title: {
-    color: '#fff',
+    color: COLORS.white,
+  },
+  default: {
+    width: 'auto',
   },
   small: {
     width: 100,
     height: 40,
   },
-  large: {
+  medium: {
     width: 200,
     height: 40,
   },
 });
-
 
 export default styles;
