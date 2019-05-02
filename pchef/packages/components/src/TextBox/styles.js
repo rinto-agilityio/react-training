a0ab3a8ade65c3378dd22cddd7e2581a69ace560
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-02 17:01:35 +0700

    Handle textbox component

---
+++ pchef/packages/components/src/TextBox/styles.js
@@ -0,0 +1,20 @@
+// Libs
+import { StyleSheet } from 'react-native';
+
+// Themes
+import { COLORS } from '../../../themes/Colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flex: 1
+  },
+  default: {
+    height: 40,
+    borderBottomColor: COLORS.inputBorder,
+    borderBottomWidth: 1,
+    fontSize: 18,
+    flex: 1,
+    color: COLORS.black
+  },
+});
