e33f00917f2e42284bdb333957b54e6a4e70df4b
Linh Tran <linh.tran@asnet.com.vn>
2019-05-02 11:05:01 +0700

    Create constants

---
+++ pchef/packages/mobile/src/constants/api.js
@@ -0,0 +1,22 @@
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+} from 'react-native-dotenv';
+
+// config values
+export const KEY = API_KEY;
+export const DOMAIN = AUTH_DOMAIN;
+export const URL = DATABASE_URL;
+export const ID = PROJECT_ID;
+export const BUCKET = STORAGE_BUCKET;
+export const SENDER_ID = MESSAGING_SENDER_ID;
+
+// refs
+export const REF_USERS = 'users';
+
+// events
+export const CHILD_ADDED = 'child_added';
