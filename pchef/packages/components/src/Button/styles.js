0ee3b4fdfaec3741890a9243a74e9a15ae48e4a5
Quan Do <quan.do@asnet.com.vn>
2019-05-02 17:13:00 +0700

    Update height for button

--- pchef/packages/components/src/Button/styles.js
+++ pchef/packages/components/src/Button/styles.js
@@ -16,15 +16,12 @@ const styles = StyleSheet.create({
     color: COLORS.white,
   },
   small: {
-    minHeight: 30,
     padding: METRICS.smallPadding,
   },
   medium: {
-    minHeight: 40,
     padding: METRICS.mediumPadding,
   },
   large: {
-    minHeight: 40,
     padding: METRICS.largePadding,
   },
 });
