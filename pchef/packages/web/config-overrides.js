adc31b65e69b5e128b8aac2c7d07ca71919894aa
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-03 17:27:19 +0700

    Add stash config for RNE

--- pchef/packages/web/config-overrides.js
+++ pchef/packages/web/config-overrides.js
@@ -1,31 +1,48 @@
 const fs = require('fs')
 const path = require('path')
-const webpack = require('webpack')
+// const webpack = require('webpack')
+
+const {override, addBabelPlugins, babelInclude} = require("customize-cra");
 
 const appDirectory = fs.realpathSync(process.cwd())
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
 // our packages that will now be included in the CRA build step
-const appIncludes = [
-  resolveApp('src'),
-  resolveApp('../components/src'),
-]
+// const appIncludes = [
+//   resolveApp('src'),
+//   resolveApp('../components/src'),
+//   resolveApp('../../node_modules/react-native-elements'),
+//   resolveApp('../../node_modules/react-native-vector-icons'),
+// ]
 
-module.exports = function override(config, env) {
-  // allow importing from outside of src folder
-  config.resolve.plugins = config.resolve.plugins.filter(
-    plugin => plugin.constructor.name !== 'ModuleScopePlugin'
-  )
-  config.module.rules[0].include = appIncludes
-  config.module.rules[1] = null
-  config.module.rules[2].oneOf[1].include = appIncludes
-  config.module.rules[2].oneOf[1].options.plugins = [
-    require.resolve('babel-plugin-react-native-web'),
-  ].concat(config.module.rules[2].oneOf[1].options.plugins)
-  config.module.rules = config.module.rules.filter(Boolean)
-  config.plugins.push(
-    new webpack.DefinePlugin({ __DEV__: env !== 'production' })
-  )
+// module.exports = function override(config, env) {
+//   // allow importing from outside of src folder
+//   config.resolve.plugins = config.resolve.plugins.filter(
+//     plugin => plugin.constructor.name !== 'ModuleScopePlugin'
+//   )
+//   config.module.rules[0].include = appIncludes
+//   config.module.rules[1] = null
+//   config.module.rules[2].oneOf[1].include = appIncludes
+//   config.module.rules[2].oneOf[1].options.plugins = [
+//     require.resolve('babel-plugin-react-native-web'),
+//     // require.resolve('@babel/plugin-proposal-class-properties'),
+//   ].concat(config.module.rules[2].oneOf[1].options.plugins)
+//   config.module.rules = config.module.rules.filter(Boolean)
+//   config.plugins.push(
+//     new webpack.DefinePlugin({ __DEV__: env !== 'production' })
+//   )
 
-  return config
-}
+//   return config
+// }
+
+module.exports = override(
+  babelInclude([
+    resolveApp('src'),
+    resolveApp('../components/src'),
+    resolveApp('../../node_modules/react-native-elements'),
+    resolveApp('../../node_modules/react-native-vector-icons'),
+  ]),
+  addBabelPlugins(
+    '@babel/plugin-proposal-class-properties',
+  ),
+);
