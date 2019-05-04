e043d8bad1b97f168b197a22646f811422ffe7b7
Quan Do <quan.do@asnet.com.vn>
2019-05-04 16:48:30 +0700

    Update config storybook

--- pchef/packages/web/.storybook/config.js
+++ pchef/packages/web/.storybook/config.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 
 import { configure } from '@storybook/react';
 
-const req = require.context('../src', true, /\.stories\.js$/);
+const req = require.context('../src/storybook', true, /\.stories\.js$/);
 
 const loadStories = () => {
   _.sortBy(req.keys(), [filename => _.last(filename.split('/'))]).forEach(
