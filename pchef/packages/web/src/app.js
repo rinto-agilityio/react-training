1ec4612baad61e904f29c399cc8badfabf2eb12c
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-04 16:56:01 +0700

    Change import path for shared source

--- pchef/packages/web/src/app.js
+++ pchef/packages/web/src/app.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 // Components
-import AppComponent from 'components/src/components/App'
-import Counting from 'components/src/components/Counting'
-import Button from 'components/src/components/Button'
-import Icon from 'components/src/components/Icon'
+import AppComponent from 'shared/src/components/App'
+import Counting from 'shared/src/components/Counting'
+import Button from 'shared/src/components/Button'
+import Icon from 'shared/src/components/Icon'
 
 const App = () => (
   <>
