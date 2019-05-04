50f706266877242f524936f4e6c15f2a1e09847e
Linh Tran <linh.tran@asnet.com.vn>
2019-05-04 09:48:51 +0700

    Ignore some warnings

--- pchef/packages/mobile/App.js
+++ pchef/packages/mobile/App.js
@@ -3,8 +3,15 @@ import { ApolloClient } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { createRtdbLink } from 'apollo-link-firebase';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { YellowBox } from 'react-native';
 import firebase from '@configs/firebase';
 import AppRouter from '@router/index';
+
+// ignore warnings
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+]);
 
 // create Realtime Database link
 const rtdbLink = createRtdbLink({
