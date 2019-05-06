489a9c6aa52f53a36cb507175bb0feb38778f554
Linh Tran <linh.tran@asnet.com.vn>
2019-05-06 18:10:31 +0700

    Config apollo client

--- pchef/packages/mobile/App.js
+++ pchef/packages/mobile/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { ApolloClient } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import { createRtdbLink } from 'apollo-link-firebase';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import firebase from '@configs/firebase';
 import AppRouter from '@router/index';
+import { WebSocketLink } from 'apollo-link-ws';
+import { createHttpLink } from 'apollo-link-http';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import * as constants from '@constants/api';
 
-// create Realtime Database link
-const rtdbLink = createRtdbLink({
-  database: firebase.database(),
+// create web sockket link
+const wsLink = new WebSocketLink({
+  uri: constants.WS_URI,
+  options: {
+    reconnect: true,
+    timeout: 30000,
+  },
 });
+
+// create http link server
+const httpLink = createHttpLink({
+  uri: constants.SERVER_URI,
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink
+);
 
 // create client
 const client = new ApolloClient({
-  link: rtdbLink,
+  link,
   cache: new InMemoryCache(),
 });
 
