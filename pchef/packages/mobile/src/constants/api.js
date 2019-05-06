477baf9cb757f11a71ccd15fc84a48a4219c332f
Linh Tran <linh.tran@asnet.com.vn>
2019-05-06 18:10:05 +0700

    Update api constants

--- pchef/packages/mobile/src/constants/api.js
+++ pchef/packages/mobile/src/constants/api.js
@@ -1,19 +1,8 @@
-import {
-  API_KEY,
-  AUTH_DOMAIN,
-  DATABASE_URL,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-} from 'react-native-dotenv';
+import { WS, SERVER } from 'react-native-dotenv';
 
 // config values
-export const KEY = API_KEY;
-export const DOMAIN = AUTH_DOMAIN;
-export const URL = DATABASE_URL;
-export const ID = PROJECT_ID;
-export const BUCKET = STORAGE_BUCKET;
-export const SENDER_ID = MESSAGING_SENDER_ID;
+export const WS_URI = WS;
+export const SERVER_URI = SERVER;
 
 // refs
 export const REF_USERS = 'users';
