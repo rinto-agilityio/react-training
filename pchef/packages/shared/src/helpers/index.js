53ad3922ee9d870ca4d9bf73c17be47931e2deb1
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-06 15:40:37 +0700

    Add helpers for shared components

---
+++ pchef/packages/shared/src/helpers/index.js
@@ -0,0 +1,36 @@
+/**
+ * Get full date for calendar by format YYYY-MM-DD
+ */
+export const getDateForCalendar = date => {
+  const dayFormat = new Date(date)
+  const year = dayFormat.getFullYear()
+  const month = dayFormat.getMonth() + 1
+  const monthFormat = `${month < 10 ? 0 : ''}${month}`
+  const day = `${dayFormat.getDate() < 10 ? 0 : ''}${dayFormat.getDate()}`
+
+  return `${year}-${monthFormat}-${day}`
+}
+
+/**
+ * Getting the date of next week
+ * @param {Boolean} isGetMonday
+ */
+export const getDateOfWeek = () => {
+  const day = new Date()
+
+  // Get end day of current week
+  const endOfWeek = day.setDate(day.getDate() - (day.getDay() - 1) + 6)
+
+  let startOfNextWeek = new Date(endOfWeek)
+  // Set start of next week with = end of current week + 1 day
+  startOfNextWeek = startOfNextWeek.setDate(startOfNextWeek.getDate() + 1)
+
+  let endOfNextWeek = new Date(startOfNextWeek)
+  // Set end of next week with = start of next week + 6 days
+  endOfNextWeek = endOfNextWeek.setDate(endOfNextWeek.getDate() + 6)
+
+  return {
+    minDate: getDateForCalendar(startOfNextWeek),
+    maxDate: getDateForCalendar(endOfNextWeek)
+  }
+}
