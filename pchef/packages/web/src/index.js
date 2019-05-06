85950b3ba2880ed205d2e0abac8b810d0fe83547
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-06 15:42:54 +0700

    Update font icon for web

--- pchef/packages/web/src/index.js
+++ pchef/packages/web/src/index.js
@@ -3,6 +3,25 @@ import { AppRegistry } from 'react-native'
 
 import App from './app'
 
+// Generate required css
+import iconFont from 'react-native-vector-icons/Fonts/FontAwesome.ttf';
+const iconFontStyles = `@font-face {
+  src: url(${iconFont});
+  font-family: FontAwesome;
+}`;
+
+// Create stylesheet
+const style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet) {
+  style.styleSheet.cssText = iconFontStyles;
+} else {
+  style.appendChild(document.createTextNode(iconFontStyles));
+}
+
+// Inject stylesheet
+document.head.appendChild(style);
+
 AppRegistry.registerComponent('myprojectname', () => App)
 AppRegistry.runApplication('myprojectname', {
   rootTag: document.getElementById('root')
