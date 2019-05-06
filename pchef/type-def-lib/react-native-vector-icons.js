59c06489b568d3cbbdec6ea465c8369f4b622756
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-06 18:08:57 +0700

    Update type-dev-lib

--- pchef/type-def-lib/react-native-vector-icons.js
+++ pchef/type-def-lib/react-native-vector-icons.js
@@ -1,3 +1,7 @@
+declare module 'react-native-vector-icons/FontAwesome' {
+  declare module.exports: any;
+}
+
 declare module 'react-native-vector-icons/Fonts/FontAwesome.ttf' {
   declare module.exports: any;
 }
