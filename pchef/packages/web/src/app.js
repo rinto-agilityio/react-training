38b471d4ff5fb06fcf5c25b9b1da24583f6834cc
Nguyet Tran <nguyet.tran@asnet.com.vn>
2019-05-06 11:02:49 +0700

    Remove unused code

--- pchef/packages/web/src/app.js
+++ pchef/packages/web/src/app.js
@@ -5,13 +5,11 @@ import AppComponent from 'shared/src/components/App'
 import Counting from 'shared/src/components/Counting'
 import Button from 'shared/src/components/Button'
 import Icon from 'shared/src/components/Icon'
-import Layout from 'shared/src/components/Layout'
 
 const App = () => (
   <>
     <Counting />
     <Icon />
-    <Layout type='row' />
     <AppComponent />
     <Button title="Click me" size="small" />
   </>
