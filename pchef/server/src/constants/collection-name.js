b9add6d71c9a1a073bca9e8747c3870197b0202a
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-07 14:34:12 +0700

    Add constants for collection name on firebase

---
+++ pchef/server/src/constants/collection-name.js
@@ -0,0 +1,12 @@
+// Collection name on Firebase
+const USER = 'users'
+const CATEGORY = 'categories'
+const COOKING_TYPE = 'cooking_types'
+const RECIPE = 'recipes'
+
+module.exports = {
+  USER,
+  CATEGORY,
+  COOKING_TYPE,
+  RECIPE
+}
