87223953a5185360018c89d8ebeb3a3a95412e0a
Quan Do <quan.do@asnet.com.vn>
2019-05-07 18:17:08 +0700

    Create component recipe in screen category

---
+++ pchef/packages/shared/src/screens/category/components/Recipe.js
@@ -0,0 +1,84 @@
+// libs
+import React from 'react';
+import { View, Text } from 'react-native';
+import Wrapper from '../../../layout/Wrapper'
+import Icon from '../../../components/Icon'
+import Image from '../../../components/Image'
+
+// Themes
+import { COLORS, METRICS } from '../../../themes'
+
+// Styles
+import styles from './styles';
+
+type Props = {
+  title?: string,
+  image?: string,
+  description?: string,
+  onClickIcon?: () => void,
+  size?: string,
+  color?: string,
+  handleTouch?: () => void
+}
+
+const Recipe = ({
+  title,
+  image,
+  description,
+  onClickIcon,
+  size = "",
+  color,
+  handleTouch
+}: Props) => (
+    <View style={[
+      styles[`${size}Content`]
+    ]}>
+      <Text
+        style={[
+          styles.title,
+          styles[`${size}Title`]
+        ]}
+      >
+        {title}
+      </Text>
+      <Wrapper direction="row" childPosition="left">
+        <Image
+          url={{ uri: image }}
+          customImageStyle={[
+            styles.image,
+            styles[`${size}Image`]
+          ]}
+          handleTouch={handleTouch}
+        />
+        <Wrapper
+          direction="column"
+          childPosition="middle"
+          customStyles={[
+            styles.wrapper,
+            styles[`${size}Wrapper`]
+          ]}
+        >
+          <Text style={[styles.text, styles[`${size}Text`]]}>{description}</Text>
+          <Icon
+            wrapperIconStyle={styles.icon}
+            name="heart"
+            size={size === 'large' ? METRICS.fontSize.xLarge : METRICS.fontSize.medium}
+            onClick={onClickIcon}
+            color={color}
+          />
+        </Wrapper>
+      </Wrapper>
+    </View>
+  )
+
+Recipe.defaultProps = {
+  title: '',
+  image: '',
+  description: '',
+  onClickIcon: () => {},
+  size: 'medium',
+  color: COLORS.grayDarker,
+  handleTouch: () => {}
+}
+
+export default Recipe
---
+++ pchef/packages/shared/src/screens/category/components/styles.js
@@ -0,0 +1,59 @@
+// Lib
+import { StyleSheet, Dimensions } from 'react-native';
+
+// Define screen width
+const screenWidth = Math.round(Dimensions.get('window').width);
+
+// Thmemes
+import { METRICS } from '../../../themes'
+
+// Create style for Recipe
+const styles = StyleSheet.create({
+  wrapper: {
+    height: 150,
+    width: screenWidth - 170,
+    marginLeft: METRICS.mediumMargin,
+    marginRight: METRICS.mediumMargin,
+  },
+  text: {
+    overflow: 'hidden',
+    height: 130,
+    fontSize: METRICS.fontSize.base
+  },
+  image: {
+    width: 150,
+    height: 150
+  },
+  icon: {
+    position: 'absolute',
+    bottom: 10,
+    right: 15
+  },
+  largeContent: {
+    width: 700,
+    height: 400,
+    position: 'relative',
+  },
+  largeWrapper: {
+    width: 380,
+    height: 300,
+    paddingLeft: METRICS.xLargePadding,
+  },
+  largeImage: {
+    width: 300,
+    height: 300,
+  },
+  title: {
+    fontSize: METRICS.fontSize.large,
+    paddingBottom: METRICS.mediumPadding
+  },
+  largeTitle: {
+    fontSize: METRICS.fontSize.xLarge,
+  },
+  largeText: {
+    fontSize: METRICS.fontSize.medium,
+    lineHeight: 25
+  }
+});
+
+export default styles;
