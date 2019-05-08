83beed35578b6e351f0796a6f39085b4c3dcbe96
Huyen Pham <huyen.pham@asnet.com.vn>
2019-05-08 15:35:28 +0700

    Install react router

---
+++ pchef/type-def-lib/web-alias.js
@@ -0,0 +1,23 @@
+declare module 'pages/Home' {
+  declare module.exports: any
+}
+
+declare module 'pages/Profile' {
+  declare module.exports: any
+}
+
+declare module 'pages/Login' {
+  declare module.exports: any
+}
+
+declare module 'pages/Category' {
+  declare module.exports: any
+}
+
+declare module 'pages/RecipeDetail' {
+  declare module.exports: any
+}
+
+declare module 'router/MainRouter' {
+  declare module.exports: any
+}
