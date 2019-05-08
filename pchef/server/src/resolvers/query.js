d0aee071bb63cded94fdfff19b214f2e40eb2119
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-08 15:38:05 +0700

    Refactor server code follow ESLint rules

--- pchef/server/src/resolvers/query.js
+++ pchef/server/src/resolvers/query.js
@@ -11,103 +11,106 @@ const {
   getCollectionWithConditions,
 } = require('../helpers/firestore')
 
+/* eslint-disable no-unused-vars */
+// Disable no-unused-vars for authenticated need context to get UserInfo
+
 const Query = {
   // Category
-  getCategory: authenticated((_, { id }, {}) => {
+  getCategory: authenticated((_, { id }, context) => {
     const queryConditions = [
       {
         fieldName: 'categoryId',
         operator: '==',
-        value: id
-      }
+        value: id,
+      },
     ]
 
     return getDocument(`${COLLECTION_NAME.CATEGORY}/${id}`)
-      .then(category => {
-        return getCollectionWithConditions(
+      .then(category => (
+        getCollectionWithConditions(
           COLLECTION_NAME.RECIPE,
-          queryConditions
+          queryConditions,
         )
-        .then(recipes => {
-          category.recipes = recipes
-
-          return category
-        })
-      })
+          .then(recipes => ({
+            ...category,
+            recipes,
+          }))
+      ))
   }),
 
-  getAllCategories: authenticated((_, args, {}) => {
-    return getCollection('categories')
-      .then(categories => {
-        return map(categories, category => {
+  getAllCategories: authenticated((_, args, context) => (
+    getCollection('categories')
+      .then(categories => (
+        map(categories, category => {
           const queryConditions = [
             {
               fieldName: 'categoryId',
               operator: '==',
-              value: category.id
-            }
+              value: category.id,
+            },
           ]
 
           return getCollectionWithConditions(
             COLLECTION_NAME.RECIPE,
             queryConditions,
           )
-          .then(recipes => {
-            category.recipes = recipes
-
-            return category
-          })
+            .then(recipes => ({
+              ...category,
+              recipes,
+            }))
         })
-      })
-  }),
+      ))
+  )),
 
   // Recipe
-  getRecipe: authenticated((_, { id }, {}) => {
-    return getDocument(`${COLLECTION_NAME.RECIPE}/${id}`)
-  }),
+  getRecipe: authenticated((_, { id }, context) => (
+    getDocument(`${COLLECTION_NAME.RECIPE}/${id}`)
 
-  getAllRecipes: authenticated((_, args, context) => {
-    return getCollection(COLLECTION_NAME.RECIPE)
-  }),
+  )),
+
+  getAllRecipes: authenticated((_, args, context) => (
+    getCollection(COLLECTION_NAME.RECIPE)
+  )),
 
   // RecipeStep
-  getRecipeStep: authenticated((_, { id }, {}) => {
-    return getDocument(`${COLLECTION_NAME.RECIPE_STEP}/${id}`)
-  }),
+  getRecipeStep: authenticated((_, { id }, context) => (
+    getDocument(`${COLLECTION_NAME.RECIPE_STEP}/${id}`)
+  )),
 
-  getAllRecipeSteps: authenticated((_, { id }, {}) => {
+  getAllRecipeSteps: authenticated((_, { id }, context) => {
     const queryConditions = [
       {
         fieldName: 'recipeId',
         operator: '==',
-        value: id
-      }
+        value: id,
+      },
     ]
 
     return getCollectionWithConditions(
-        COLLECTION_NAME.RECIPE_STEP,
-        queryConditions
-      )
+      COLLECTION_NAME.RECIPE_STEP,
+      queryConditions,
+    )
       .then(steps => steps)
       .catch(error => error)
   }),
 
-  getRecipeComments: authenticated((_, { id }, {}) => {
+  getRecipeComments: authenticated((_, { id }, context) => {
     const queryConditions = [
       {
         fieldName: 'recipeId',
         operator: '==',
-        value: id
-      }
+        value: id,
+      },
     ]
 
     return getCollectionWithConditions(
       COLLECTION_NAME.COMMENT,
-      queryConditions
+      queryConditions,
     )
-    .then(comments => comments)
-    .catch(error => error)
+      .then(comments => comments)
+      .catch(error => error)
   }),
 }
+/* eslint-enable */
 
 module.exports = Query
