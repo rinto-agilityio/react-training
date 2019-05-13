c093ee918c080ba02266e6af74faaa8208e90264
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-13 14:51:31 +0700

    Implment storybook for shared component

--- pchef/packages/new-shared/.storybook/config.js
+++ pchef/packages/new-shared/.storybook/config.js
@@ -2,13 +2,7 @@ import React from 'react';
 
 import { configure } from '@storybook/react';
 
-// function loadStories() {
-//   require('../src/stories');
-// }
-
-// configure(loadStories, module);
-
-const req = require.context('../src/stories', true, /\.story\.js$/)
+const req = require.context('../src', true, /\.story\.js$/)
 
 function loadStories() {
   req.keys().forEach(req)
