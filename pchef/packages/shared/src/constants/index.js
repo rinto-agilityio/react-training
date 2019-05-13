3fc47804e53ffe6e4377c395c0f32314948e1e35
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-13 11:58:34 +0700

    Update colors variables

--- pchef/packages/shared/src/constants/index.js
+++ pchef/packages/shared/src/constants/index.js
@@ -1,3 +1,5 @@
 export const CALENDAR_TITLE_FORMAT = 'MMMM yyyy'
 
 export const SEPARATOR_SPLIT_STRING = ','
+
+export const LIMIT_STEPS_IN_PROGRESS = 5
--- pchef/packages/shared/src/themes/Colors.js
+++ pchef/packages/shared/src/themes/Colors.js
@@ -14,4 +14,5 @@ export default {
   baseBlue: '#42a5f5',
   lightBlue: 'rgba(228, 228, 228, 0.42)',
   darkBlue: '#2d4150',
+  blackLighter: 'rgba(0, 0, 0, 0.71)',
 }
