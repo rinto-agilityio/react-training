17e94c85601ff1cb2a6902378c8e057d8e04fca5
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-14 10:24:01 +0700

    Write image background storybook

---
+++ pchef/packages/new-shared/src/components/ImageBackground/ImageBg.story.js
@@ -0,0 +1,26 @@
+// Libs
+import React from 'react'
+import { Text, View } from 'react-native'
+import { storiesOf } from '@storybook/react'
+
+// Themes
+import { METRICS, COLORS } from '../../themes'
+
+// Components
+import ImageBackground from '.'
+
+storiesOf('ImageBackground', module)
+  .add('text children', () => (
+    <View
+      style={{
+        width: METRICS.largeBgImage,
+        height: METRICS.largeBgImage,
+      }}
+    >
+      <ImageBackground
+        url="https://vcdn-ngoisao.vnecdn.net/2018/01/19/1-8515-1516314392.jpg"
+      >
+        <Text style={{ color: COLORS.white }}>Text</Text>
+      </ImageBackground>
+    </View>
+  ))
