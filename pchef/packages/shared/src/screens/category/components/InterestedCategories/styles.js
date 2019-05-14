acbefc622fdd9de7448d2d80666ba2e4762737f5
Linh Tran <linh.tran@asnet.com.vn>
2019-05-14 11:04:15 +0700

    Remove unuse style

--- pchef/packages/shared/src/screens/category/components/InterestedCategories/styles.js
+++ pchef/packages/shared/src/screens/category/components/InterestedCategories/styles.js
@@ -1,42 +1,19 @@
-import { StyleSheet, Dimensions } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { METRICS, COLORS } from '../../../../themes'
-
-// get device dimension
-const width = Math.round(Dimensions.get('window').width)
-
-// get item dimension base on device width
-const itemDimension = (width - 4) / 3
 
 export default StyleSheet.create({
   container: {
     margin: -1,
-  },
-  secondaryContainer: {
-    width: METRICS.categoryContainer,
+    minHeight: 300,
   },
   item: {
     margin: 1,
-  },
-  primaryImage: {
-    width: itemDimension,
-    height: itemDimension,
-  },
-  secondaryImage: {
-    width: METRICS.categoryItem,
-    height: METRICS.categoryItem,
+    position: 'relative',
   },
   layer: {
     position: 'absolute',
     backgroundColor: COLORS.black,
     zIndex: 1,
-  },
-  primaryLayer: {
-    width: itemDimension,
-    height: itemDimension,
-  },
-  secondaryLayer: {
-    width: METRICS.categoryItem,
-    height: METRICS.categoryItem,
   },
   activeLayer: {
     opacity: 0.2,
