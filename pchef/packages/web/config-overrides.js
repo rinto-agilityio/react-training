a9c0ef8864e656bbec586f826fbee823ea20d5ba
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-14 10:27:21 +0700

    Update override config for local package instead of monorepos

--- pchef/packages/web/config-overrides.js
+++ pchef/packages/web/config-overrides.js
@@ -12,17 +12,24 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 module.exports = override(
   babelInclude([
     resolveApp('src'),
-    resolveApp('../shared/src'),
+
+    // Local package for shared-component
+    resolveApp('./node_modules/pchef-shared'), // Will be removed after migrated
+    resolveApp('./node_modules/pchef-newshared'),
+
     // 3rd lib: react-native-elements
-    resolveApp('../../node_modules/react-native-elements'),
-    resolveApp('../../node_modules/react-native-ratings'),
-    resolveApp('../../node_modules/react-native-vector-icons'),
+    resolveApp('./node_modules/react-native-paper'),
+    resolveApp('./node_modules/react-native-safe-area-view'),
+    resolveApp('./node_modules/react-native-elements'), // Will be removed after migrated
+    resolveApp('./node_modules/react-native-ratings'),
+    resolveApp('./node_modules/react-native-vector-icons'),
 
     // 3rd lib: react-native-calendars
-    resolveApp('../../node_modules/react-native-calendars'),
+    resolveApp('./node_modules/react-native-calendars'),
   ]),
   addBabelPlugins(
     "@babel/plugin-proposal-class-properties",
+    '@babel/plugin-proposal-object-rest-spread',
     "babel-plugin-react-native-web",
   )
 )
