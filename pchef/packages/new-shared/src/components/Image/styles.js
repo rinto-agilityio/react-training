dc4fc1c5e95616d743347560aeb1eb5036dc5cce
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-14 09:00:15 +0700

    Write Image test

---
+++ pchef/packages/new-shared/src/components/Image/styles.js
@@ -0,0 +1,14 @@
+// Libs
+import { StyleSheet } from 'react-native'
+
+// Themes
+import { METRICS } from '../../themes'
+
+const styles = StyleSheet.create({
+  image: {
+    width: METRICS.mediumInput,
+    height: METRICS.mediumInput,
+  },
+})
+
+export default styles
