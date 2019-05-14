05604485ddf04dabc1cd4ceb9d19598d0d337572
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-14 16:29:36 +0700

    Write storybook and test for reaction component

---
+++ pchef/packages/new-shared/src/components/Reaction/Reaction.test.js
@@ -0,0 +1,14 @@
+// Components
+import Reaction from '.'
+
+it('renders correctly', () => {
+  const reaction = renderer.create(
+    <Reaction
+      votes={[1]}
+      size="medium"
+      isFavorited
+    />,
+  ).toJSON()
+
+  expect(reaction).toMatchSnapshot()
+});
---
+++ pchef/packages/new-shared/src/components/Reaction/styles.js
@@ -0,0 +1,16 @@
+// Libs
+import { StyleSheet } from 'react-native'
+
+// Themes
+import { METRICS } from '../../themes'
+
+const styles = StyleSheet.create({
+  wrapperIcon: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: METRICS.largePadding,
+  },
+})
+
+export default styles
