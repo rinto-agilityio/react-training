4b7aa89abd60a6beab9cbc742f9919e6b86e9aee
Linh Tran <linh.tran@asnet.com.vn>
2019-05-14 16:20:45 +0700

    Remove unuse config

--- pchef/client/mobile/metro.config.js
+++ pchef/client/mobile/metro.config.js
@@ -7,9 +7,6 @@
 const path = require('path')
 
 module.exports = {
-  resolver: {
-    blacklistRE: /node_modules\/.*\/node_modules\/react-native\/.*/,
-  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
