d0124f7b7ae6e660db40ae46f95265889eae1a44
Rin To <rin.to@asnet.com.vn>
2019-05-14 17:58:03 +0700

    Write unit test for PrivateRoute component

---
+++ blog-app/client/webapp/src/utils/PrivateRoute.test.js
@@ -0,0 +1,38 @@
+// Libs
+import { MockedProvider } from 'react-apollo/test-utils';
+import { BrowserRouter } from 'react-router-dom';
+
+// Components
+import PrivateRoute from './PrivateRoute';
+import { LOGGED_USER } from '../graphql/author/queries';
+
+it('Return PrivateRoute snapshots', () => {
+  const loggedUser = {
+    id: '1557377599679',
+    email: 'rin.to@gmail.com',
+    password: '123456',
+    name: 'rinto'
+  };
+
+  const mocks = [
+    {
+      request: {
+        query: LOGGED_USER
+      },
+      result: {
+        data: {
+          loggedUser: loggedUser
+        }
+      }
+    }
+  ];
+  const component = mount(
+    <BrowserRouter>
+      <MockedProvider mocks={mocks}>
+        <PrivateRoute />
+      </MockedProvider>
+    </BrowserRouter>
+  );
+
+  expect(component).toMatchSnapshot();
+});
