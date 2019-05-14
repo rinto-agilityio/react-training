028baf11f7ad18cdc5c07ca33fd42f804a0ed437
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-14 10:27:44 +0700

    Try to call shared-component

--- pchef/packages/web/src/pages/Home/index.js
+++ pchef/packages/web/src/pages/Home/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+// Component
+// $FlowFixMe
+import BtnPaper from 'pchef-newshared/src/components/Button'
+
 const Home = () => (
   <>
     <h3>Home page</h3>
     <Link to="profile">Link to Profile</Link>
     <Link to="category">Link to Category</Link>
+    <BtnPaper />
   </>
 )
 
