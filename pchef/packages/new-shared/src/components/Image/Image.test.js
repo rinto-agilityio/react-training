487863587f110b743864a5c633e3c77d263877a9
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-14 10:02:02 +0700

    Move image story to image component

--- pchef/packages/new-shared/src/components/Image/Image.test.js
+++ pchef/packages/new-shared/src/components/Image/Image.test.js
@@ -1,16 +1,13 @@
-// Libs
-import 'react-native'
-import React from 'react'
-import renderer from 'react-test-renderer'
-
 // Components
 import Image from '.'
 
 it('renders correctly', () => {
-  renderer.create(
+  const image = renderer.create(
     <Image
       url="https://vcdn-ngoisao.vnecdn.net/2018/01/19/1-8515-1516314392.jpg"
       disabled
     />,
-  )
+  ).toJSON()
+
+  expect(image).toMatchSnapshot()
 });
