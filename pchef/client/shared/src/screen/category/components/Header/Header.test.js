b13b0a974159a7d807e0020e42047de29f557fb4
Huyen Pham <huyen.pham@asnet.com.vn>
2019-05-15 18:17:48 +0700

    Test for Header component

---
+++ pchef/client/shared/src/screen/category/components/Header/Header.test.js
@@ -0,0 +1,37 @@
+// Libs
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+// Components
+import Header from '.'
+
+// Mock
+import { category } from '../../../../mocks'
+
+const props = {
+  category,
+}
+
+describe('Components', () => {
+  describe('Header', () => {
+    let component
+
+    beforeEach(() => {
+      component = shallow(<Header {...props} />)
+    })
+
+    it('should render Header component with grid', () => {
+      const renderComponent = renderer.create(<Header {...props} isGrid />)
+      expect(renderComponent).toMatchSnapshot()
+    })
+
+    it('should render Header component without grid', () => {
+      expect(
+        component
+          .find('Icon')
+          .at(0)
+          .props().color,
+      ).toEqual('#c5c5c5')
+    })
+  })
+})
