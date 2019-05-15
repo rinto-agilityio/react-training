5cae10bb431f13866b03b5fd6165933c2c5143bf
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-15 16:51:04 +0700

    Write storybook and test for calendar component

---
+++ pchef/client/shared/src/components/Calendar/styles.js
@@ -0,0 +1,15 @@
+// Lib
+import { StyleSheet } from 'react-native'
+
+// Themes
+import { COLORS } from '../../themes'
+
+// Create style for calendar
+const styles = StyleSheet.create({
+  calendar: {
+    borderWidth: 1,
+    borderColor: COLORS.grayDarker,
+  },
+})
+
+export default styles
