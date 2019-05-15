af34fbb32794de1cbc37cd22634b21ebf592d4ea
Huyen Pham <huyen.pham@asnet.com.vn>
2019-05-15 18:17:27 +0700

    Using mock data

--- pchef/client/shared/src/screen/category/components/Header/Header.story.js
+++ pchef/client/shared/src/screen/category/components/Header/Header.story.js
@@ -5,11 +5,8 @@ import { storiesOf } from '@storybook/react'
 // Components
 import Header from '.'
 
-const category = {
-  name: 'Heathy',
-  imgUrl:
-    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-}
+// Mock
+import { category } from '../../../../mocks'
 
 storiesOf('Header', module).add('default', () => (
   <Header category={category} isGrid size="large" />
