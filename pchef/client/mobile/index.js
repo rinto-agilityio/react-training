3534b2baa3bf8b6b1a8e2e7d6d6debac07c42989
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-15 14:47:33 +0700

    Update storybook config

--- pchef/client/mobile/index.js
+++ pchef/client/mobile/index.js
@@ -7,3 +7,8 @@ import App from './App'
 import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
+
+// TODO: Need a flag config for storybook or run app
+// import Storybook from './storybook'
+
+// AppRegistry.registerComponent(appName, () => Storybook)
