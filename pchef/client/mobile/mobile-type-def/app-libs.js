12b8e9cd3f41dd6f304a9d74b9d9b418a74b0a71
Linh Tran <linh.tran@asnet.com.vn>
2019-05-15 12:04:38 +0700

    Create mobile-type-def

---
+++ pchef/client/mobile/mobile-type-def/app-libs.js
@@ -0,0 +1,129 @@
+// Apollo boost
+declare module 'apollo-boost' {
+  declare module.exports: any
+}
+
+// Apollo cache memory
+declare module 'apollo-cache-inmemory' {
+  declare module.exports: any
+}
+
+// Apollo link http
+declare module 'apollo-link-http' {
+  declare module.exports: any
+}
+
+// Apollo link ws
+declare module 'apollo-link-ws' {
+  declare module.exports: any
+}
+
+// Apollo link
+declare module 'apollo-link' {
+  declare module.exports: any
+}
+
+// Apollo utilities
+declare module 'apollo-utilities' {
+  declare module.exports: any
+}
+
+declare module 'customize-cra' {
+  declare module.exports: any
+}
+
+// Apollo boost
+declare module 'fromnow' {
+  declare module.exports: any
+}
+
+// React apollo
+declare module 'react-apollo' {
+  declare module.exports: any
+}
+
+// react-router-dom
+declare module 'react-router-dom' {
+  declare module.exports: any
+}
+
+// react-native
+declare module 'react-native' {
+  declare module.exports: any
+}
+
+// react native calendars
+declare module 'react-native-calendars' {
+  declare module.exports: any
+}
+
+// react native vector icons - font awesome
+declare module 'react-native-vector-icons/FontAwesome' {
+  declare module.exports: any
+}
+
+declare module 'react-native-vector-icons/Fonts/FontAwesome.ttf' {
+  declare module.exports: any
+}
+
+// TODO: Remove this RNE
+declare module 'react-native-elements' {
+  declare module.exports: any
+}
+
+// react-native paper
+declare module 'react-native-paper' {
+  declare module.exports: any
+}
+
+// font material
+declare module 'react-native-vector-icons/Fonts/MaterialIcons.ttf' {
+  declare module.exports: any
+}
+
+// react-native-dotenv
+declare module 'react-native-dotenv' {
+  declare module.exports: any
+}
+
+// react navigation
+declare module 'react-navigation' {
+  declare module.exports: any
+}
+
+// react test renderer
+declare module 'react-test-renderer' {
+  declare module.exports: any
+}
+
+// enzyme adaper
+declare module 'enzyme-adapter-react-16' {
+  declare module.exports: any
+}
+
+// enzyme
+declare module 'enzyme' {
+  declare module.exports: any
+}
+
+// jsdom
+declare module 'jsdom' {
+  declare module.exports: any
+}
+
+// storybook libs
+declare module '@storybook/react' {
+  declare module.exports: any
+}
+
+declare module '@storybook/addon-actions' {
+  declare module.exports: any
+}
+
+declare module '@storybook/addon-links' {
+  declare module.exports: any
+}
+
+declare module '@storybook/react/demo' {
+  declare module.exports: any
+}
