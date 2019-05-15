9e169d1801c86e34c8e941c2060d6769fb9f0cb7
Rin To <rin.to@asnet.com.vn>
2019-05-15 17:22:03 +0700

    Write unit test for SignUp component

---
+++ blog-app/client/webapp/src/pages/authentication/SignUp.test.js
@@ -0,0 +1,110 @@
+import { MockedProvider } from 'react-apollo/test-utils';
+import { CREATE_USER } from '../../graphql/author/mutations';
+import { Form } from 'react-bootstrap';
+import SignUp from './SignUp';
+import Indicator from '../../components/commons/Indicator';
+import waitForExpect from 'wait-for-expect';
+import { BrowserRouter} from 'react-router-dom';
+
+const mocks = [
+  {
+    request: {
+      query: CREATE_USER,
+      variables: {
+        id: '1557799396370',
+        email: 'rinto0503@gmail.com',
+        password: '123456',
+        name: 'rin to'
+      }
+    },
+    result: {
+      data: {
+        signUp: {
+          success: true,
+          message: 'signup success',
+          author: {
+            id: '1557799396370',
+            email: 'rinto0503@gmail.com',
+            password: '123456',
+            name: 'rin to'
+          }
+        }
+      }
+    }
+  }
+];
+
+describe('Test SignUp', () => {
+
+  it('Testing SignUp Loading state:', () => {
+
+    const renderComponent = mount(
+      <BrowserRouter>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <SignUp />
+        </MockedProvider>
+      </BrowserRouter>
+    );
+
+    renderComponent.find(Form).simulate('submit', {
+      preventDefault: jest.fn()
+    });
+
+    expect(renderComponent.find(Indicator).length).toEqual(1);
+  });
+
+  it('Testing SignUp Final state:', async () => {
+
+    const renderComponent = mount(
+      <BrowserRouter>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <SignUp />
+        </MockedProvider>
+      </BrowserRouter>
+    );
+
+    renderComponent.find(Form).simulate('submit', {
+      preventDefault: jest.fn()
+    });
+
+    await waitForExpect(() => {
+      renderComponent.update();
+      expect(renderComponent.find(Form).exists()).toBeTruthy();
+    });
+  });
+
+  it('Testing SignUp Final state:', async () => {
+    const mocksError = [
+      {
+        request: {
+          query: CREATE_USER,
+          variables: {
+            id: '1557799396370',
+            email: 'rinto0503@gmail.com',
+            password: '123456',
+            name: 'rin to'
+          }
+        },
+        error:  new Error('error')
+      }
+    ];
+    const renderComponent = mount(
+      <BrowserRouter>
+        <MockedProvider mocks={mocksError} addTypename={false}>
+          <SignUp />
+        </MockedProvider>
+      </BrowserRouter>
+    );
+
+    renderComponent.find(Form).simulate('submit', {
+      preventDefault: jest.fn()
+    });
+
+    await waitForExpect(() => {
+      renderComponent.update();
+
+      expect(renderComponent.find(Form).exists()).toBeTruthy();
+    });
+  });
+
+});
