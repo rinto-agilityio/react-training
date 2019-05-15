6aafa3fe268950308159eb5eb76d83e93b6caf21
Linh Tran <linh.tran@asnet.com.vn>
2019-05-15 14:16:23 +0700

    Add example use shared component

--- pchef/client/mobile/src/screens/Login/index.js
+++ pchef/client/mobile/src/screens/Login/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 
+// Component
+import BtnPaper from 'pchef-shared/src/components/Button'
+
 // define Login page styles
 const styles = StyleSheet.create({
   container: {
@@ -13,6 +16,10 @@ const Login = () => (
   <View style={styles.container}>
     <Text>test</Text>
     <Text>This is login screen</Text>
+    <BtnPaper
+      onPress={() => console.log('Press BtnPaper')}
+      title="BtnPaper"
+    />
   </View>
 )
 
