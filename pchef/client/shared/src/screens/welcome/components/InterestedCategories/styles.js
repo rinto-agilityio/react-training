275b125449fbc785c5794424e5ec7d846d91d353
Linh Tran <linh.tran@asnet.com.vn>
2019-05-15 15:55:27 +0700

    Create unit testing for InterestedCategories component

---
+++ pchef/client/shared/src/screens/welcome/components/InterestedCategories/styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from 'react-native'
+import { METRICS, COLORS, FONTS } from '../../../../themes'
+
+const styles = StyleSheet.create({
+  container: {
+    margin: -1,
+    minHeight: 300,
+  },
+  item: {
+    margin: 1,
+    position: 'relative',
+  },
+  layer: {
+    position: 'absolute',
+    backgroundColor: COLORS.black,
+    zIndex: 1,
+  },
+  activeLayer: {
+    opacity: METRICS.activeItem,
+  },
+  inactiveLayer: {
+    opacity: METRICS.inactiveItem,
+  },
+  name: {
+    position: 'absolute',
+    fontWeight: 'bold',
+    zIndex: 2,
+  },
+  activeName: {
+    color: COLORS.white,
+  },
+  inactiveName: {
+    color: COLORS.halfWhite,
+  },
+  primaryName: {
+    left: METRICS.smallMargin,
+    top: 8,
+    fontSize: FONTS.fontSize.base,
+  },
+  secondaryName: {
+    left: METRICS.mediumMargin,
+    top: METRICS.smallMargin + METRICS.mediumMargin,
+    fontSize: FONTS.fontSize.large,
+  },
+})
+
+export default styles
