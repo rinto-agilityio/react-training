fbe7c55b75f6ca8a7e1b614c8f63f0ffc571cfdd
Huyen Pham <huyen.pham@asnet.com.vn>
2019-05-15 18:17:27 +0700

    Using mock data

---
+++ pchef/client/shared/src/mocks/index.js
@@ -0,0 +1,6 @@
+// mock categories
+export const category = {
+  name: 'Heathy',
+  imgUrl:
+    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+}
