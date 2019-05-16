ff6fc90b4bb60a8624e4261016a0aa4e57d3200f
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-16 17:52:40 +0700

    Improve coverage test recipe

--- pchef/client/shared/src/screens/recipe/components/Comment/Comment.test.js
+++ pchef/client/shared/src/screens/recipe/components/Comment/Comment.test.js
@@ -2,26 +2,30 @@
 import Comment from '.'
 
 describe('Comment', () => {
-  it('renders correctly', () => {
-    const comment = renderer.create(
-      <Comment
-        avatarUri="https://vcdn-ngoisao.vnecdn.net/2018/01/19/1-8515-1516314392.jpg"
-        name="Andy"
-      />,
-    ).toJSON()
+  const props = {
+    avatarUri: 'https://vcdn-ngoisao.vnecdn.net/2018/01/19/1-8515-1516314392.jpg',
+    name: 'Andy',
+  }
 
-    expect(comment).toMatchSnapshot()
+  const component = mount(<Comment {...props} />)
+  it('renders correctly', () => {
+    expect(component).toMatchSnapshot()
   })
 
-  it('renders correctly user comment', () => {
-    const comment = renderer.create(
-      <Comment
-        avatarUri="https://vcdn-ngoisao.vnecdn.net/2018/01/19/1-8515-1516314392.jpg"
-        name="Andy"
-        isGetTime
-      />,
-    ).toJSON()
+  it('renders correctly component with isGetTime = true', () => {
+    component.setProps({
+      isGetTime: true,
+      type: 'primary',
+    })
 
-    expect(comment).toMatchSnapshot()
+    expect(component).toMatchSnapshot()
+  })
+
+  it('renders correctly component with type secondary', () => {
+    component.setProps({
+      type: 'secondary',
+    })
+
+    expect(component).toMatchSnapshot()
   })
 })
--- pchef/client/shared/src/screens/recipe/components/Comment/index.js
+++ pchef/client/shared/src/screens/recipe/components/Comment/index.js
@@ -29,7 +29,7 @@ const Comment = ({
   name,
   text,
   customStyle,
-  type = 'primary',
+  type,
   publishedAt,
   isGetTime,
   customNameStyles,
