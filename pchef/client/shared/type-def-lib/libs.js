0b37b217cad03b7292ecea08ebe78f984be743f9
Huy Nguyen Van <huy.nguyenvan@asnet.com.vn>
2019-05-16 14:44:25 +0700

    Add type-def-lib

---
+++ pchef/client/shared/type-def-lib/libs.js
@@ -0,0 +1,12 @@
+// Apollo Client
+declare module 'apollo-boost' {
+  declare module.exports: any
+}
+
+declare module 'react-apollo' {
+  declare module.exports: any
+}
+
+declare module 'graphql-tag' {
+  declare module.exports: any
+}
