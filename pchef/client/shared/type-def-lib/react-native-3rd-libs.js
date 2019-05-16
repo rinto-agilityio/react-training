2dda595802510a7e9f6d2fc64324245c408de027
Nhung Duong <nhung.duong@asnet.com.vn>
2019-05-16 10:01:57 +0700

    Add type-def-lib folder

---
+++ pchef/client/shared/type-def-lib/react-native-3rd-libs.js
@@ -0,0 +1,15 @@
+declare module 'react-native' {
+  declare module.exports: any;
+}
+
+declare module 'react-native-calendars' {
+  declare module.exports: any;
+}
+
+declare module 'react-native-paper' {
+  declare module.exports: any;
+}
+
+declare module 'react-native-vector-icons/Fonts/MaterialIcons.ttf' {
+  declare module.exports: any;
+}
---
+++ pchef/client/shared/type-def-lib/storybook.js
@@ -0,0 +1,15 @@
+declare module '@storybook/react' {
+  declare module.exports: any;
+}
+
+declare module '@storybook/addon-actions' {
+  declare module.exports: any;
+}
+
+declare module '@storybook/addon-links' {
+  declare module.exports: any;
+}
+
+declare module '@storybook/react/demo' {
+  declare module.exports: any;
+}
