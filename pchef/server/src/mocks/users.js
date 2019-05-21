a915077e0bf497581e34c8af7c136059fbdf8565
Huyen Pham <huyen.pham@asnet.com.vn>
2019-05-21 18:02:31 +0700

    Create mock user on server

---
+++ pchef/server/src/mocks/users.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    id: '123',
+    email: 'h@gmail.com',
+    name: 'huyen',
+    avatar: 'dd',
+    favoriteRecipe: ['Efe6eOMWozQAVpCxzhr7', 'jHD0SLRw1wMAuosI6ruH'],
+    followCategory: ['2uAi2sb1IkPxpKPzvDNn', '3xjHYbeO7D4b4UhjKeWU'],
+  },
+]
+
+module.exports = users
