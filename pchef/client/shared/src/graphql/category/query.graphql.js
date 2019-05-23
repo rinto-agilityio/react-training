a7bf35ab3350d5579a71db1fcc9a615abef40161
Huynh Vo <huynh.vo@asnet.com.vn>
2019-05-23 17:37:47 +0700

    Create getCategory query function

---
+++ pchef/client/shared/src/graphql/category/query.graphql.js
@@ -0,0 +1,36 @@
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const GET_CATEGORY = gql`
+  query getCategory($id: String!) {
+    getCategory(id: $id) {
+      id
+      imgUrl
+      recipes {
+        id
+        categoryId
+        cookingTypeId
+        title
+        isDraft
+        subTitle
+        imgUrl
+        description
+        views
+        votes
+        modifyDate
+        publishedDate
+      }
+    }
+  }
+`
+
+export const getCategory = graphql(GET_CATEGORY, {
+  options: props => ({
+    variables: { id: props.id },
+  }),
+  props: ({ data }) => {
+    const { loading, error, getCategory } = data
+    const { category, recipes } = getCategory
+    return { loading, error, category, recipes }
+  },
+})
