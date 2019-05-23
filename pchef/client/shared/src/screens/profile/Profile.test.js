a3e849c8f80afad058c3ebe4aa7a1c8fdbaa89ac
Huyen Pham <huyen.pham@asnet.com.vn>
2019-05-23 18:28:56 +0700

    Update test for tabs and Profile

--- pchef/client/shared/src/screens/profile/Profile.test.js
+++ pchef/client/shared/src/screens/profile/Profile.test.js
@@ -16,9 +16,25 @@ const props = {
 
 describe('Screen', () => {
   describe('Profile', () => {
+    let component
+
+    beforeEach(() => {
+      component = shallow(<Profile {...props} />)
+    })
+
     it('should render Category component with grid', () => {
       const renderComponent = renderer.create(<Profile {...props} />)
       expect(renderComponent).toMatchSnapshot()
+    })
+
+    it('should render Loading component when query is loading', () => {
+      component.setProps({ loading: true })
+      expect(component.find('Loading')).toHaveLength(1)
+    })
+
+    it('should render Error when query failed', () => {
+      component.setProps({ error: 'Failed' })
+      expect(component.find('Error')).toHaveLength(1)
     })
   })
 })
