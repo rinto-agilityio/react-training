96bb63cfd80c263ec030e23aa1c6844600f0c885
Binh Tran <binh.tran@asnet.com.vn>
2019-05-29 10:26:32 +0700

    Create container for Wishlist form

---
+++ pchef/client/shared/src/containers/WishListForm.js
@@ -0,0 +1,8 @@
+import { compose } from 'react-apollo'
+
+// GraphQL
+import { getAllCookingTypes, getAllCategories } from '../graphql/recipe'
+import { createWishList } from '../graphql/wish-list'
+import WishListForm from '../screens/wish-list/components/WishListForm'
+
+export default compose(getAllCookingTypes, getAllCategories, createWishList )(WishListForm)
