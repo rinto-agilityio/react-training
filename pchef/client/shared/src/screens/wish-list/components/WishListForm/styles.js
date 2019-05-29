0476e5d86020b5ade03fe56be0d299de28532339
Binh Tran <binh.tran@asnet.com.vn>
2019-05-29 10:24:22 +0700

    Update style for Wishlist

--- pchef/client/shared/src/screens/wish-list/components/WishListForm/styles.js
+++ pchef/client/shared/src/screens/wish-list/components/WishListForm/styles.js
@@ -2,14 +2,42 @@
 import { StyleSheet } from 'react-native'
 
 // Themes
-import { METRICS, FONTS } from '../../../../themes'
+import { METRICS, FONTS, COLORS } from '../../../../themes'
 
 const styles = StyleSheet.create({
   // Container
   container: {
     ...METRICS.flexCenter,
   },
-
+  wrapperIcon: {
+    backgroundColor: COLORS.baseGray,
+    padding: 20,
+    marginTop: METRICS.mediumMargin,
+  },
+  wrapperClassifyIcon: {
+    paddingBottom: 0,
+  },
+  icon: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: COLORS.green,
+    borderRadius: METRICS.mediumBorderRadius,
+    ...METRICS.flexCenter,
+    paddingRight: METRICS.mediumPadding,
+  },
+  classifyIcon: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginBottom: METRICS.largeMargin,
+  },
+  wrapperMainPhoto: {
+    ...METRICS.flexCenter,
+    flexDirection: 'column',
+    backgroundColor: COLORS.green,
+    marginTop: METRICS.mediumMargin,
+    marginBottom: METRICS.mediumMargin,
+    paddingBottom: METRICS.mediumPadding,
+  },
   // Dialog
   // Width full container for small/medium dialog
   largeDialog: {
