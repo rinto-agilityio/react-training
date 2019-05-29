6891bb934085cf79e3067c37ef1bcd2a848d16bd
Binh Tran <binh.tran@asnet.com.vn>
2019-05-29 11:01:18 +0700

    Update code style for container

--- pchef/client/shared/src/containers/WishListForm.js
+++ pchef/client/shared/src/containers/WishListForm.js
@@ -5,4 +5,7 @@ import { getAllCookingTypes, getAllCategories } from '../graphql/recipe'
 import { createWishList } from '../graphql/wish-list'
 import WishListForm from '../screens/wish-list/components/WishListForm'
 
-export default compose(getAllCookingTypes, getAllCategories, createWishList )(WishListForm)
+export default compose(
+  getAllCookingTypes,
+  getAllCategories,
+  createWishList )( WishListForm )
