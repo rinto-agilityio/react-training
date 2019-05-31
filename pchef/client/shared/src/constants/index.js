9225cf14f1c88e0ff9780c8da8ea7070cf49c52b
Huynh Vo <huynh.vo@asnet.com.vn>
2019-05-31 16:19:41 +0700

    Define and apply column of view mode

--- pchef/client/shared/src/constants/index.js
+++ pchef/client/shared/src/constants/index.js
@@ -4,6 +4,9 @@ export const SEPARATOR_SPLIT_STRING = ','
 
 export const LIMIT_STEPS_IN_PROGRESS = 5
 
+export const GRID_VIEW_COLUMN = 3
+export const LIST_VIEW_COLUMN = 1
+
 // validate pattern
 export const PATTERN = {
   EMAIL: /^[a-zA-Z0-9_.+-]+@asnet.com.vn$/,
--- pchef/client/shared/src/screens/category/index.js
+++ pchef/client/shared/src/screens/category/index.js
@@ -1,6 +1,9 @@
 // Libs
 import React, { useState } from 'react'
-import { Platform, View, FlatList, Button } from 'react-native'
+import { Platform, View, FlatList } from 'react-native'
+
+// Constant
+import { GRID_VIEW_COLUMN, LIST_VIEW_COLUMN } from '../../constants/index'
 
 // Components
 import Header from './components/Header'
@@ -34,21 +37,20 @@ const CategoryScreen = ({
   error,
 }: Props) => {
   const size = Platform.OS === 'web' ? 'large' : 'small'
-  const [columns, setColumns] = useState(1)
+  const [columns, setColumns] = useState(LIST_VIEW_COLUMN)
   const [isGrid, setIsGrid] = useState(false)
 
   if (loading) return <Loading size="small" />
   if (error) return <Error message={error.message} size={size} />
 
   const handleSelectListView = itemName => {
-    console.log('itemName = ', itemName)
     if (itemName === 'view-list') {
       // view List
-      setColumns(1)
+      setColumns(LIST_VIEW_COLUMN)
       setIsGrid(false)
     } else {
       // view Grid
-      setColumns(3)
+      setColumns(GRID_VIEW_COLUMN)
       setIsGrid(true)
     }
   }
