8e83e4cc4d12f45b33a0313be59a52aaabd636e3
Rin To <rin.to@asnet.com.vn>
2019-05-31 17:56:00 +0700

    Config private route for web app

--- pchef/client/web/src/router/MainRouter.js
+++ pchef/client/web/src/router/MainRouter.js
@@ -3,19 +3,25 @@ import React from 'react'
 // router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+
 // components
 import Home from 'pages/Home'
 import Profile from 'pages/Profile'
-import Login from 'pages/Login'
+import LoginContainer from 'pchef-shared/src/containers/Login'
 import Category from 'pages/Category'
+
+// helpers
+import PrivateRoute from '../helpers/PrivateRoute'
+
+const loginPath = '/login'
 
 const MainRouter = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/category" component={Category} />
-      <Route exact path="/profile" component={Profile} />
+      <Route exact path={loginPath} component={LoginContainer} />
+      <PrivateRoute exact path="/" loginPath={loginPath} component={Home} />
+      <PrivateRoute exact loginPath={loginPath} path="/category" component={Category} />
+      <PrivateRoute exact loginPath={loginPath} path="/profile" component={Profile} />
     </Switch>
   </Router>
 )
