d590603f8198803bde1ea7ea0d18745e759540a1
Rin To <rin.to@asnet.com.vn>
2019-05-31 09:14:30 +0700

    Updated test case

--- pchef/client/shared/src/screens/recipe-step/components/Recipe/Recipe.test.js
+++ pchef/client/shared/src/screens/recipe-step/components/Recipe/Recipe.test.js
@@ -22,33 +22,56 @@ describe('Recipe by step', () => {
     onPressStep: jest.fn(),
     onPressSelectStep: jest.fn(),
   }
-  const recipeComponent = renderer.create(<Recipe {...recipeProps} />).toJSON()
-  const progressStepComponent = shallow(<ProgressStep {...stepProps} />)
 
   it('Renders correctly recipe commponent', () => {
+    const recipeComponent = renderer.create(<Recipe {...recipeProps} />).toJSON()
     expect(recipeComponent).toMatchSnapshot()
   })
 
   it('Renders correctly progress steps commponent', () => {
+    const progressStepComponent = renderer.create(<ProgressStep {...stepProps} />).toJSON
     expect(progressStepComponent).toMatchSnapshot()
   })
 
+  it('Render recipe component with size medium', () => {
+    const props = {
+      ...recipeProps,
+      size: 'medium',
+    }
+    const recipeComponent = renderer.create(<Recipe {...props} />).toJSON()
+    expect(recipeComponent).toMatchSnapshot()
+  })
+
   it('Render progress step component with steps array', () => {
-    progressStepComponent.setProps({
-      steps: recipe.steps,
-    })
+    const props = {
+      ...stepProps,
+      size: 'medium',
+    }
+    const progressStepComponent = renderer.create(<ProgressStep {...props} />).toJSON()
     expect(progressStepComponent).toMatchSnapshot()
   })
 
   it('Function props of recipe component should be defined', () => {
-    Recipe.defaultProps.onPress()
-    expect(Recipe.defaultProps.onPress).toBeDefined()
+    const props = {
+      ...recipeProps,
+      size: 'medium',
+    }
+
+    const recipeComponent = renderer.create(<Recipe {...props} />).toJSON()
+
+    recipeComponent.props.onPress()
+    expect(Recipe.props.onPress).toBeDefined()
   })
 
   it('Function props of progress step component should be defined', () => {
-    ProgressStep.defaultProps.onPressStep()
-    ProgressStep.defaultProps.onPressSelectStep()
-    expect(ProgressStep.defaultProps.onPressStep).toBeDefined()
-    expect(ProgressStep.defaultProps.onPressSelectStep).toBeDefined()
+    const props = {
+      ...stepProps,
+      size: 'medium',
+    }
+    const ProgressStep = renderer.create(<ProgressStep {...props} />).toJSON()
+    ProgressStep.props.onPressStep()
+    ProgressStep.props.onPressSelectStep()
+    expect(ProgressStep.props.onPressStep).toBeDefined()
+    expect(ProgressStep.props.onPressSelectStep).toBeDefined()
   })
 })
