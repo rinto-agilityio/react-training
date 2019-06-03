701f85babd34f3ca45505b1c1eac673f39855b1d
Nhung Duong <nhung.duong@asnet.com.vn>
2019-06-03 14:06:16 +0700

    Update styles for textbox component

--- pchef/client/shared/src/components/TextBox/styles.js
+++ pchef/client/shared/src/components/TextBox/styles.js
@@ -5,17 +5,12 @@ import { StyleSheet } from 'react-native'
 import { COLORS, METRICS, FONTS } from '../../themes'
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    flex: 1,
-  },
   default: {
     height: METRICS.mediumInput,
     borderColor: COLORS.grayDarker,
     borderBottomWidth: 1,
     fontSize: FONTS.fontSize.medium,
     color: COLORS.black,
-    flex: 1,
   },
 })
 
