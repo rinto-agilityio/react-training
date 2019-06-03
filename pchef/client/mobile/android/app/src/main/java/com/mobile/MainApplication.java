26f4fab523bc7b48795c0c7b6a430b195c1793d4
Nhung Duong <nhung.duong@asnet.com.vn>
2019-06-03 16:47:24 +0700

    Link react native libs and add fonts icon for mobile

--- pchef/client/mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ pchef/client/mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -3,6 +3,9 @@ package com.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
+            new VectorIconsPackage(),
+            new ImagePickerPackage(),
             new RNGestureHandlerPackage()
       );
     }
