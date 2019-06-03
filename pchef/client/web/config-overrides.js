f017725faa4bcc9d6d8cc03e27ad351ab0cd51ce
Rin To <rin.to@asnet.com.vn>
2019-06-03 18:21:45 +0700

    Updated override config

--- pchef/client/web/config-overrides.js
+++ pchef/client/web/config-overrides.js
@@ -1,31 +1,27 @@
-const fs = require('fs')
-const path = require('path')
-const { override, addBabelPlugins, babelInclude } = require('customize-cra')
+module.exports = function override(config) {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: '/node_modules/pchef-shared/',
+    use: {
+      loader: 'babel-loader',
+      options: {
+        // Disable reading babel configuration
+        babelrc: false,
+        configFile: false,
 
-const appDirectory = fs.realpathSync(process.cwd())
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
+        // The configration for compilation
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react',
+          '@babel/preset-flow',
+        ],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-object-rest-spread',
+        ],
+      },
+    },
+  })
 
-module.exports = override(
-  babelInclude([
-    resolveApp('src'),
-
-    // Local package for shared-component
-    resolveApp('./node_modules/pchef-shared'), // Will be removed after migrated
-    resolveApp('./node_modules/pchef-newshared'),
-
-    // 3rd lib: react-native-elements
-    resolveApp('./node_modules/react-native-paper'),
-    resolveApp('./node_modules/react-native-safe-area-view'),
-    resolveApp('./node_modules/react-native-elements'), // Will be removed after migrated
-    resolveApp('./node_modules/react-native-ratings'),
-    resolveApp('./node_modules/react-native-vector-icons'),
-
-    // 3rd lib: react-native-calendars
-    resolveApp('./node_modules/react-native-calendars'),
-  ]),
-  addBabelPlugins(
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    'babel-plugin-react-native-web',
-  ),
-)
+  return config
+}
