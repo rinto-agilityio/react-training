3da7efba0eaec70bd3be3b5aab344bb230762de2
Huynh Vo <huynh.vo@asnet.com.vn>
2019-06-03 15:46:50 +0700

    Define ELLIPSIS_LINE constant

--- pchef/client/shared/src/constants/index.js
+++ pchef/client/shared/src/constants/index.js
@@ -7,6 +7,8 @@ export const LIMIT_STEPS_IN_PROGRESS = 5
 export const GRID_VIEW_COLUMN = 3
 export const LIST_VIEW_COLUMN = 1
 
+export const ELLIPSIS_LINE = 1
+
 // validate pattern
 export const PATTERN = {
   EMAIL: /^[a-zA-Z0-9_.+-]+@asnet.com.vn$/,
