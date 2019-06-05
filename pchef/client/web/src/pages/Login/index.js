044b2893accb312f1c61f9b4f64a259f9d59c5c7
Rin To <rin.to@asnet.com.vn>
2019-06-05 08:50:38 +0700

    Fixed flow

--- pchef/client/web/src/pages/Login/index.js
+++ pchef/client/web/src/pages/Login/index.js
@@ -3,11 +3,16 @@ import React from 'react'
 // Containers
 import LoginContainer from 'pchef-shared/src/containers/Login'
 
-const Login = ({ history }) => {
+type Props = {
+  history: Object
+}
+
+const Login = ({ history }: Props) => {
   const handlingLoginSuccess = (token: string) => {
     localStorage.setItem('token', token)
     history.push('/')
   }
+
   return (
     <>
       <h3>Login page</h3>
