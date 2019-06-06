b840da06efb37684f1bde1f158e8bdd9e4071741
Huynh Vo <huynh.vo@asnet.com.vn>
2019-06-06 14:30:09 +0700

    Import more query for Category screen

--- pchef/client/shared/src/containers/Category.js
+++ pchef/client/shared/src/containers/Category.js
@@ -2,6 +2,10 @@ import { compose } from 'react-apollo'
 
 // import graphQL query + component
 import { getCategory } from '../graphql/category'
+import { userToggleRecipe } from '../graphql/recipe'
+import { getRecipes } from '../graphql/recipe'
+
+// import component
 import CategoryScreen from '../screens/category'
 
-export default compose(getCategory)(CategoryScreen)
+export default compose(getCategory, userToggleRecipe, getRecipes)(CategoryScreen)
