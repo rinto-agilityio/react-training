c1111901d0f2f1070cd6a8a052e0c464df72b1a0
Binh Tran <binh.tran@asnet.com.vn>
2019-06-06 11:58:47 +0700

    Create WishlistFormContainer

---
+++ pchef/client/web/src/pages/WishListForm/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Containers
+import WishListFormContainer from 'pchef-shared/src/containers/WishListForm'
+
+const WishList = () => (
+  <WishListFormContainer />
+)
+
+export default WishList
