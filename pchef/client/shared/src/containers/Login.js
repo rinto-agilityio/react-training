44ea7fa209846427a319090c4d7ce9e26abe0617
Binh Tran <binh.tran@asnet.com.vn>
2019-06-07 13:57:46 +0700

    Update Login container

--- pchef/client/shared/src/containers/Login.js
+++ pchef/client/shared/src/containers/Login.js
@@ -1,7 +1,7 @@
 import { compose } from 'react-apollo'
 
 // GraphQL
-import { signInUser } from '../graphql/user'
+import { signInUser, getUser } from '../graphql/user'
 import Login from '../screens/login'
 
-export default compose(signInUser)(Login)
+export default compose(signInUser, getUser)(Login)
