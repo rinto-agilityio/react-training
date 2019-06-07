40640cf191fe9435d0624c3fb427feaaf01867bd
Nhung Duong <nhung.duong@asnet.com.vn>
2019-06-07 17:55:39 +0700

    Install react-native-fetch-blob

--- pchef/client/mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ pchef/client/mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -3,6 +3,8 @@ package com.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
+            new RNFetchBlobPackage(),
             new AsyncStoragePackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
