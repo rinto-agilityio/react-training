2ce8b2758d2831969669a022c520e5e9b4e306a2
Huynh Vo <huynh.vo@asnet.com.vn>
2019-06-11 09:01:39 +0700

    Style for tabs to view recipe list and most votes recipes

--- pchef/client/shared/src/screens/new-feed/styles.js
+++ pchef/client/shared/src/screens/new-feed/styles.js
@@ -2,7 +2,7 @@
 import { StyleSheet } from 'react-native'
 
 // Themes
-import { METRICS } from '../../themes'
+import { METRICS, COLORS } from '../../themes'
 
 // Create style for Header
 const styles = StyleSheet.create({
@@ -14,6 +14,24 @@ const styles = StyleSheet.create({
     width: METRICS.extraLargeScreen,
     marginRight: 'auto',
     marginLeft: 'auto',
+  },
+  tabListContainer: {},
+  tabWrap: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderBottomWidth: METRICS.smallBorderWidth,
+    borderColor: COLORS.baseGray,
+    margintop: METRICS.largeMargin,
+  },
+  tab: {
+    borderWidth: 2,
+    borderRadius: METRICS.largeBorderRadius,
+    borderColor: COLORS.red,
+    backgroundColor: 'green',
+    paddingTop: METRICS.mediumPadding,
+    paddingBottom: METRICS.mediumPadding,
+    paddingRight: METRICS.mediumPadding,
+    paddingLeft: METRICS.mediumPadding,
   },
 })
 
