ccebd44794d1271ad26eeada773c225cec856dcd
Huynh Vo <huynh.vo@asnet.com.vn>
2019-06-11 17:48:22 +0700

    Remove unused code

--- pchef/client/shared/src/App.js
+++ pchef/client/shared/src/App.js
@@ -18,7 +18,6 @@ import client from './config/apollo-client'
 
 // Containers
 import LoginContainer from './containers/Login'
-import NewFeedContainer from './containers/NewFeed'
 
 type AppProps = {}
 
@@ -37,11 +36,10 @@ export default class App extends Component<AppProps> {
             <Text style={styles.welcome}>
               Shared-components for both Web and Mobile
             </Text>
-            {/* <LoginContainer
+            <LoginContainer
               type="secondary"
               handlingLoginSuccess={this.handlingLoginSuccess}
-            /> */}
-            <NewFeedContainer type="secondary" />
+            />
           </View>
         </PaperProvider>
       </ApolloProvider>
