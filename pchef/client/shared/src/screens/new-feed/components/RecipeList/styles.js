63f8357e275076c9df7b9aa3c110b28482373bf9
Huynh Vo <huynh.vo@asnet.com.vn>
2019-06-11 17:14:44 +0700

    Style for two tab recipe list

--- pchef/client/shared/src/screens/new-feed/components/RecipeList/styles.js
+++ pchef/client/shared/src/screens/new-feed/components/RecipeList/styles.js
@@ -2,7 +2,7 @@
 import { StyleSheet } from 'react-native'
 
 // Themes
-import { METRICS } from '../../../../themes'
+import { METRICS, COLORS } from '../../../../themes'
 
 // Create style for Header
 const styles = StyleSheet.create({
@@ -23,6 +23,31 @@ const styles = StyleSheet.create({
   },
   indicator: {
     marginTop: METRICS.largeMargin * 10,
+  },
+  tabListContainer: {},
+  tabWrap: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderBottomWidth: METRICS.smallBorderWidth,
+    borderColor: COLORS.baseGray,
+    marginTop: METRICS.largeMargin,
+  },
+  tab: {
+    borderWidth: 2,
+    borderColor: 'black',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingRight: 20,
+    paddingLeft: 20,
+    display: 'inline-block',
+  },
+  tabViewActive: {
+    backgroundColor: COLORS.red,
+  },
+  tabView: {
+    backgroundColor: COLORS.baseGray,
   },
 })
 
