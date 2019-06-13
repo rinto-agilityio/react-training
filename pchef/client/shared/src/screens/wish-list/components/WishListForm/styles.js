f4e787825740f1acf2f856b9efd8fd8a0c859325
Binh Tran <binh.tran@asnet.com.vn>
2019-06-13 09:54:09 +0700

    Update style for Wishlist form

--- pchef/client/shared/src/screens/wish-list/components/WishListForm/styles.js
+++ pchef/client/shared/src/screens/wish-list/components/WishListForm/styles.js
@@ -36,6 +36,9 @@ const styles = StyleSheet.create({
     marginBottom: METRICS.mediumMargin,
     paddingBottom: METRICS.mediumPadding,
   },
+  wrapper: {
+    zIndex: METRICS.iconZindex,
+  },
   // Dialog
   // Width full container for small/medium dialog
   largeDialog: {
