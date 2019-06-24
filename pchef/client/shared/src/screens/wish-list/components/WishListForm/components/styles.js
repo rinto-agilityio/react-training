e38e9675e68095a15ed32a028f9e98b879e55456
Binh Tran <binh.tran@asnet.com.vn>
2019-06-24 14:57:51 +0700

    Update style for Wishlist form

---
+++ pchef/client/shared/src/screens/wish-list/components/WishListForm/components/styles.js
@@ -0,0 +1,47 @@
+// Libs
+import { StyleSheet } from 'react-native'
+
+// Themes
+import { METRICS, FONTS, COLORS } from '../../../../../themes'
+
+const styles = StyleSheet.create({
+  // Wrapper
+  wrapper: {
+    zIndex: METRICS.iconZindex,
+  },
+
+  // Title
+  title: {
+    ...METRICS.truncate,
+    color: COLORS.black,
+    fontWeight: FONTS.fontWeight.large,
+    paddingLeft: METRICS.largePadding,
+    paddingRight: METRICS.largePadding,
+  },
+  smallTitle: {
+    fontSize: FONTS.fontSize.medium,
+  },
+  mediumTitle: {
+    fontSize: FONTS.fontSize.large,
+  },
+  largeTitle: {
+    fontSize: FONTS.fontSize.extraLarge,
+  },
+
+  // Image
+  image: {
+    width: '100%',
+    marginTop: METRICS.mediumMargin,
+  },
+  smallImage: {
+    height: METRICS.smallBgImage,
+  },
+  mediumImage: {
+    height: METRICS.mediumBgImage,
+  },
+  largeImage: {
+    height: METRICS.largeBgImage,
+  },
+})
+
+export default styles
