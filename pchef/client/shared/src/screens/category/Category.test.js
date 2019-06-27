961bf741213faf1546dec6fd50c67efffd857b62
Huyen Pham <huyen.pham@asnet.com.vn>
2019-06-27 18:42:21 +0700

    Test for category screen

--- pchef/client/shared/src/screens/category/Category.test.js
+++ pchef/client/shared/src/screens/category/Category.test.js
@@ -1,16 +1,33 @@
 // Libs
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { render } from 'react-native-testing-library'
+import { JSDOM } from 'jsdom'
+
+// Components
+import { FlatList } from 'react-native'
+import Recipe from './components/Recipe'
+import Category from '.'
+
 
 // Mocks
 import { categories, recipes } from '../../mocks'
 
-// Components
-import Category from '.'
+const { document } = new JSDOM('').window
+global.document = document
+global.window = document.defaultView
+global.Image = window.Image
 
 const props = {
   category: categories[0],
   recipes,
+  data: {
+    favoriteRecipe: [
+      {
+        id: 'testId',
+      },
+    ],
+  },
 }
 describe('Screen', () => {
   describe('Category', () => {
@@ -28,7 +45,12 @@ describe('Screen', () => {
     })
 
     it('should render Recipe component', () => {
-      expect(component.find('Recipe')).toHaveLength(recipes.length)
+      const componentTree = render(
+        <Category {...props} />,
+      )
+
+      expect(componentTree.getAllByType(FlatList).length).toBe(1)
+      expect(componentTree.getAllByType(Recipe).length).toBe(props.recipes.length)
     })
 
     it('should render Header component', () => {
