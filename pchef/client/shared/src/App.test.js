cf0477c2ef7fca996ee54964fa8887159a027dea
Huyen Pham <huyen.pham@asnet.com.vn>
2019-07-01 16:09:34 +0700

    Fix unit testting for App file

--- pchef/client/shared/src/App.test.js
+++ pchef/client/shared/src/App.test.js
@@ -4,13 +4,13 @@
 
 import 'react-native'
 import React from 'react'
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer'
+import ReactDOM from 'react-dom'
 
 // Components
 import App from './App'
 
 it('renders correctly', () => {
-  renderer.create(<App />)
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
--- pchef/client/shared/src/config/apollo-client.js
+++ pchef/client/shared/src/config/apollo-client.js
@@ -1,7 +1,9 @@
 import ApolloClient from 'apollo-boost'
+import fetch from 'node-fetch'
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQLURL,
+  fetch,
   request: async operation => {
     const token = await localStorage.getItem('token');
 
