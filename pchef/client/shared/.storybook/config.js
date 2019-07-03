0c4cb71a105a7eb5a3a7d218012f0654a10cfb69
Binh Tran <binh.tran@asnet.com.vn>
2019-07-03 21:26:05 +0700

    Set up addon info

--- pchef/client/shared/.storybook/config.js
+++ pchef/client/shared/.storybook/config.js
@@ -1,11 +1,20 @@
+//@flow
 import React from 'react'
 import { configure } from '@storybook/react'
+import { setDefaults } from "@storybook/addon-info";
 
 /**
  * Config Font for react-native-vector-icons
  * https://github.com/storybooks/storybook/issues/4208
  */
 
+// addon-info
+setDefaults({
+  header: true, // Toggles display of header with component name and description
+  inline: true, // Displays info inline vs component to view
+  source: true, // Displays the source of story Component
+});
+ 
 // Generate required css
 const iconFont = require('react-native-vector-icons/Fonts/FontAwesome.ttf')
 const iconFontStyles = `@font-face {
---
+++ pchef/client/shared/.storybook/utils.js
@@ -0,0 +1,18 @@
+// Libs
+import { withInfo } from '@storybook/addon-info';
+
+/**
+ * Style for header and body when using withInfo addon in storybook
+ */
+const wInfoStyle = {
+  header: {
+    h1: {
+      color: 'red',
+    },
+  },
+  infoBody: {
+    backgroundColor: 'bisque',
+  },
+}
+
+export const wInfo = () => withInfo({ styles: wInfoStyle })
