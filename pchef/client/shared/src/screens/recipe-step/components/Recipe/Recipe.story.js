42b023a555c3381ef9a1ead10df298d735df56fa
Binh Tran <binh.tran@asnet.com.vn>
2019-07-04 09:25:56 +0700

    Update storybook for Recipe

--- pchef/client/shared/src/screens/recipe-step/components/Recipe/Recipe.story.js
+++ pchef/client/shared/src/screens/recipe-step/components/Recipe/Recipe.story.js
@@ -9,6 +9,9 @@ import Recipe from '.'
 // Mocks
 import { recipes, user } from '../../../../mocks'
 
+// Helper
+import { wInfo } from '../../../../../.storybook/utils';
+
 const getUser = {
   favoriteRecipe: user.favoriteRecipe,
   user,
@@ -19,7 +22,8 @@ const getRecipe = {
 }
 
 storiesOf('Recipe by step', module)
-  .add('small', () => (
+  .addDecorator(wInfo())
+  .add('Small', () => (
     <Recipe
       size="small"
       onPress={action('clicked')}
@@ -28,7 +32,7 @@ storiesOf('Recipe by step', module)
       recipeSteps={recipes[0].steps}
     />
   ))
-  .add('medium', () => (
+  .add('Medium', () => (
     <Recipe
       size="medium"
       onPress={action('clicked')}
@@ -37,7 +41,7 @@ storiesOf('Recipe by step', module)
       recipeSteps={recipes[0].steps}
     />
   ))
-  .add('large', () => (
+  .add('Large', () => (
     <Recipe
       size="large"
       onPress={action('clicked')}
