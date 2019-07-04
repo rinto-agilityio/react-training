7c42057b0d3bb836bb26f6cebb6356beda2ac2fe
Binh Tran <binh.tran@asnet.com.vn>
2019-07-04 08:11:10 +0700

    Update for header component

--- pchef/client/shared/src/components/Header/Header.story.js
+++ pchef/client/shared/src/components/Header/Header.story.js
@@ -7,17 +7,21 @@ import { action } from '@storybook/addon-actions'
 // Component
 import Header from '.'
 
+// Helper
+import { wInfo } from '../../../.storybook/utils';
+
 const props = {
   onPressCategoryIcon: action('clicked category icon'),
   onPressLogo: action('clicked logo'),
 }
 
 storiesOf('New feed header', module)
-  .add('primary', () => (
+  .addDecorator(wInfo())
+  .add('Primary', () => (
     <View style={{ width: 500, margin: 'auto' }}>
       <Header {...props} type="primary" />
     </View>
   ))
-  .add('secondary', () => (
+  .add('Secondary', () => (
     <Header {...props} onPressCategoryIcon size="secondary" />
   ))
