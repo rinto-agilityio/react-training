22a7bb65bf59c668dd84f2fd070de53682bfe8e3
Huyen Pham <huyen.pham@asnet.com.vn>
2019-07-10 08:48:47 +0700

    Add unit testing for newfeed screen

---
+++ pchef/client/shared/src/screens/new-feed/NewFeed.test.js
@@ -0,0 +1,65 @@
+import { Platform } from 'react-native'
+
+// Components
+import NewFeed from '.'
+import Modal from '../../components/Modal'
+import Loading from '../../components/Loading'
+import Header from '../../components/Header'
+
+// Mocks
+import { categories, user, recipes } from '../../mocks'
+
+const mockPlatform = OS => {
+  jest.resetModules()
+  jest.doMock('Platform', () => ({ OS, select: objs => objs[OS] }))
+}
+
+const props = {
+  handleNavigateWelcome: jest.fn(),
+  data: {
+    favoriteRecipe: [{ id: '123' }],
+    followCategory: [{ ...categories[0], recipes }],
+    user,
+  },
+  userToggleRecipe: jest.fn(),
+  userToggleVote: jest.fn(),
+  handleClickRecipe: jest.fn(),
+  handleRedirectLogin: jest.fn(),
+  onPressCategoryPipeline: jest.fn(),
+}
+
+let component
+
+describe('NewFeed', () => {
+  beforeEach(() => {
+    component = shallow(<NewFeed {...props} />)
+  })
+
+  it('should render component', () => {
+    const component = renderer.create(<NewFeed {...props} />).toJSON()
+    expect(component).toMatchSnapshot()
+  })
+
+  it('should call handleRedirectLogin when dismiss or submit on modal', () => {
+    component.setProps({
+      error: {
+        graphQLErrors: [{ message: 'error' }],
+      },
+    })
+
+    const modal = component.find(Modal)
+    modal.props().onDismiss()
+    expect(props.handleRedirectLogin).toHaveBeenCalled()
+
+    modal.props().onSubmit()
+    expect(props.handleRedirectLogin).toHaveBeenCalled()
+  })
+
+  it('should render Loading component if loading is true', () => {
+    component.setProps({
+      loading: true,
+    })
+
+    expect(component.find(Loading)).toHaveLength(1)
+  })
+})
