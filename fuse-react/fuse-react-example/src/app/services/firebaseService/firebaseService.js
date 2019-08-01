2f3d76cae7fca10b32bf359cc02cb0e4f2d51d59
Rin To <rin.to@asnet.com.vn>
2019-08-01 16:49:32 +0700

    Config firebase

--- fuse-react/fuse-react-example/src/app/services/firebaseService/firebaseService.js
+++ fuse-react/fuse-react-example/src/app/services/firebaseService/firebaseService.js
@@ -2,15 +2,66 @@ import config from './firebaseServiceConfig'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
+import 'firebase/firestore'
 
 class firebaseService {
-  init() {
+  constructor() {
     if (firebase.apps.length) {
       return
     }
     firebase.initializeApp(config)
     this.db = firebase.database()
     this.auth = firebase.auth()
+    this.dbFileStore = firebase.firestore()
+  }
+
+  mapDocumentToEntity = doc => ({ id: doc.id, ...doc.data() })
+
+  mapCollectionToEntities = collection =>
+    collection.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+
+  getTodosData = () => {
+    if (!firebase.apps.length) {
+      return
+    }
+    const todoCollection = this.dbFileStore.collection('todos')
+    return todoCollection
+      .get()
+      .then(this.mapCollectionToEntities)
+      .catch(error => error)
+  }
+
+  getLabelsData = () => {
+    const labelsCollection = this.dbFileStore.collection('labels')
+    return labelsCollection
+      .get()
+      .then(this.mapCollectionToEntities)
+      .catch(error => error)
+  }
+
+  getFoldersData = () => {
+    const foldersCollection = this.dbFileStore.collection('folders')
+    return foldersCollection
+      .get()
+      .then(this.mapCollectionToEntities)
+      .catch(error => error)
+  }
+
+  getFiltersData = () => {
+    const filtesrCollection = this.dbFileStore.collection('filters')
+    return filtesrCollection
+      .get()
+      .then(this.mapCollectionToEntities)
+      .catch(error => error)
+  }
+
+  addNewTodoData = data => {
+    const todoCollection = this.dbFileStore.collection('todos')
+    return todoCollection
+      .doc()
+      .set(data)
+      .then(() => data)
+      .catch(error => error)
   }
 
   getUserData = userId => {
--- fuse-react/fuse-react-example/src/app/services/firebaseService/firebaseServiceConfig.js
+++ fuse-react/fuse-react-example/src/app/services/firebaseService/firebaseServiceConfig.js
@@ -1,19 +1,19 @@
 const prodConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'your-app.firebaseapp.com',
-  databaseURL: 'https://your-app.firebaseio.com',
-  projectId: 'your-app',
-  storageBucket: 'your-app.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 }
 
 const devConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'your-app.firebaseapp.com',
-  databaseURL: 'https://your-app.firebaseio.com',
-  projectId: 'your-app',
-  storageBucket: 'your-app.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 }
 
 const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
