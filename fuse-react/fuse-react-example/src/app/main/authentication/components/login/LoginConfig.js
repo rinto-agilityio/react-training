1ddc895d2296b926bd47c7561856d8ba9f6cd16b
Rin To <rin.to@asnet.com.vn>
2019-08-06 15:19:29 +0700

    Config login authentication page

---
+++ fuse-react/fuse-react-example/src/app/main/authentication/components/login/LoginConfig.js
@@ -0,0 +1,33 @@
+import Login from '../../containers/login'
+import { authRoles } from 'app/auth'
+
+export const LoginConfig = {
+  settings: {
+    layout: {
+      config: {
+        navbar: {
+          display: false,
+        },
+        toolbar: {
+          display: false,
+        },
+        footer: {
+          display: false,
+        },
+        leftSidePanel: {
+          display: false,
+        },
+        rightSidePanel: {
+          display: false,
+        },
+      },
+    },
+  },
+  auth: authRoles.onlyGuest,
+  routes: [
+    {
+      path: '/login',
+      component: Login,
+    },
+  ],
+}
