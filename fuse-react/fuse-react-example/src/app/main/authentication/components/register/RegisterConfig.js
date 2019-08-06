a6738895adc18775f2fe431142765b05a175c553
Rin To <rin.to@asnet.com.vn>
2019-08-06 15:19:47 +0700

    Config register authentication page

---
+++ fuse-react/fuse-react-example/src/app/main/authentication/components/register/RegisterConfig.js
@@ -0,0 +1,33 @@
+import { authRoles } from 'app/auth'
+import Register from '../../containers/register'
+
+export const RegisterConfig = {
+  settings: {
+    layout: {
+      config: {
+        navbar: {
+          display: false,
+        },
+        toolbar: {
+          display: false,
+        },
+        footer: {
+          display: false,
+        },
+        leftSidePanel: {
+          display: false,
+        },
+        rightSidePanel: {
+          display: false,
+        },
+      },
+    },
+  },
+  auth: authRoles.onlyGuest,
+  routes: [
+    {
+      path: '/register',
+      component: Register,
+    },
+  ],
+}
