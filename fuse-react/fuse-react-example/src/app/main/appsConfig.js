65525dca7fd8e5de519e9abe0720c1822ae4c3f2
Rin To <rin.to@asnet.com.vn>
2019-08-07 17:59:35 +0700

    Config ecommerce app

--- fuse-react/fuse-react-example/src/app/main/appsConfig.js
+++ fuse-react/fuse-react-example/src/app/main/appsConfig.js
@@ -2,10 +2,12 @@ import { TodoAppConfig } from './todo/TodoConfig'
 import { ExampleConfig } from './example/ExampleConfig'
 import { LoginConfig } from './authentication/components/login/LoginConfig'
 import { RegisterConfig } from './authentication/components/register/RegisterConfig'
+import { ECommerceAppConfig } from './e-commerce/config'
 
 export const appsConfigs = [
   ExampleConfig,
   TodoAppConfig,
   LoginConfig,
   RegisterConfig,
+  ECommerceAppConfig,
 ]
