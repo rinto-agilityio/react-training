7a4884e2c2ebfbf0ec49a7440b214e5f72329f19
Rin To <rin.to@asnet.com.vn>
2019-08-09 17:12:44 +0700

    Created login reducer for auth

---
+++ fuse-react/fuse-react-example/src/app/auth/redux/login.js
@@ -0,0 +1,48 @@
+import { createActions, createReducer } from 'reduxsauce'
+import Immutable from 'seamless-immutable'
+
+export const { Types, Creators } = createActions({
+  loginWithFirebaseProcessing: ['data'],
+  loginWithFirebaseSuccess: null,
+  loginWithFirebaseFailed: null,
+})
+
+const InitialState = Immutable({
+  success: false,
+  isProcessing: false,
+  error: {
+    username: null,
+    password: null,
+  },
+})
+
+const loginWithFirebaseProcessing = (state, action) => {
+  return state.merge({
+    isProcessing: true,
+  })
+}
+
+const loginWithFirebaseSuccess = (state, action) => {
+  return state.merge({
+    isProcessing: false,
+    success: true
+  })
+}
+
+const loginWithFirebaseFailed = (state, action) => {
+  return state.merge({
+    isProcessing: false,
+    success: false,
+    error: action.payload
+  })
+}
+
+// Assign handler to types.
+const HANDLERS = {
+  [Types.LOGIN_WITH_FIREBASE_PROCESSING]: loginWithFirebaseProcessing,
+  [Types.LOGIN_WITH_FIREBASE_SUCCESS]: loginWithFirebaseSuccess,
+  [Types.LOGIN_WITH_FIREBASE_FAILED]: loginWithFirebaseFailed,
+}
+
+// Create reducers by pass state and handlers
+export const login = createReducer(InitialState, HANDLERS)
