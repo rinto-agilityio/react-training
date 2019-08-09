7199aa71f2cf6e0e776bd5222ee8f8717c60ad21
Rin To <rin.to@asnet.com.vn>
2019-08-09 17:13:36 +0700

    Updated auth store

--- fuse-react/fuse-react-example/src/app/auth/store/reducers/index.js
+++ fuse-react/fuse-react-example/src/app/auth/store/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
-import user from './user.reducer'
-import login from './login.reducer'
+import { user } from '../../redux/user'
+// import login from './login.reducer'
 import register from './register.reducer'
+import { login } from '../../redux/login'
 
 const authReducers = combineReducers({
   user,
   login,
   register,
+  // authReducer
 })
 
 export default authReducers
