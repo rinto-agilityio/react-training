3ab45f09251d9954da24240b67ac65c218426cfc
Rin To <rin.to@asnet.com.vn>
2019-08-14 17:40:27 +0700

    Updated upload product image

--- fuse-react/fuse-react-example/src/app/main/e-commerce/components/ProductDetailTabs/ImageTab.js
+++ fuse-react/fuse-react-example/src/app/main/e-commerce/components/ProductDetailTabs/ImageTab.js
@@ -1,33 +1,46 @@
 import React from 'react'
-import classNames from "classnames";
-import {
-  Icon,
-} from "@material-ui/core"
+import classNames from 'classnames'
+import { Icon, Button } from '@material-ui/core'
 import PropTypes from 'prop-types'
 
-const ImageTab = ({ form, setFeaturedImage, classes }) => (
+const ImageTab = ({ form, setFeaturedImage, classes, handleChange }) => (
   <div>
+    <div className="flex justify-center w-full mt-20 mb-20">
+      <input
+        accept="image/*"
+        className={classes.input}
+        id="contained-button-file"
+        name="images"
+        onChange={handleChange}
+        multiple
+        type="file"
+      />
+      <label htmlFor="contained-button-file">
+        <Button variant="contained" component="span" className={classes.button}>
+          Upload Images
+        </Button>
+      </label>
+    </div>
     <div className="flex justify-center sm:justify-start flex-wrap">
-      {form.images.map(media => (
-        <div
-          onClick={() => setFeaturedImage(media.id)}
-          className={classNames(
-            classes.productImageItem,
-            "flex items-center justify-center relative w-128 h-128 rounded-4 mr-16 mb-16 overflow-hidden cursor-pointer",
-            media.id === form.featuredImageId && "featured"
-          )}
-          key={media.id}
-        >
-          <Icon className={classes.productImageFeaturedStar}>
-            star
-          </Icon>
-          <img
-            className="max-w-none w-auto h-full"
-            src={media.url}
-            alt="product"
-          />
-        </div>
-      ))}
+      {form.images &&
+        form.images.map(media => (
+          <div
+            onClick={() => setFeaturedImage(media.id)}
+            className={classNames(
+              classes.productImageItem,
+              'flex items-center justify-center relative w-128 h-128 rounded-4 mr-16 mb-16 overflow-hidden cursor-pointer',
+              media.id === form.featuredImageId && 'featured'
+            )}
+            key={media.id}
+          >
+            <Icon className={classes.productImageFeaturedStar}>star</Icon>
+            <img
+              className="max-w-none w-auto h-full"
+              src={media.url}
+              alt="product"
+            />
+          </div>
+        ))}
     </div>
   </div>
 )
