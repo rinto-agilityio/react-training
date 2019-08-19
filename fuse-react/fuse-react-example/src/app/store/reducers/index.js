45d23e88c930b96ce5f2e43a1d172a8d2b3e5a3b
Rin To <rin.to@asnet.com.vn>
2019-08-19 14:59:11 +0700

    Updated auth reducer

--- fuse-react/fuse-react-example/src/app/store/reducers/index.js
+++ fuse-react/fuse-react-example/src/app/store/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import auth from 'app/auth/store/reducers'
+import auth from 'app/auth/redux'
 import quickPanel from 'app/fuse-layouts/shared-components/quickPanel/store/reducers'
 import { eCommerceReducer } from 'app/main/e-commerce/redux'
 import fuse from './fuse'
